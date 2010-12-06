b7cc59523f728f4e71b28684c3e265ab338ad1ce
alainvizzini@gmail.com <alainvizzini@gmail.com@a4e8eee6-8666-d384-cd2c-cc5a62ec2564>
2010-12-06 14:52:31 +0000

    Added to svn

---
+++ ArdRemoteTable/uploadArdRemoteTable.Pololu.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#avrdude -c avrispv2 -p m328p -P /dev/ttyACM0 -U flash:w:ATmegaBOOT_168_atmega328.hex
+avrdude  -c avrispv2 -p m328p -P /dev/ttyACM0 -U flash:w:./hex/ArdRemoteTable.cpp.hex:i 
