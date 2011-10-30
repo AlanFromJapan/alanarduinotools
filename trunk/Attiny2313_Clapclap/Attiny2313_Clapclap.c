4755223c662cd3a9be7ab1bef2c7e19063c7dca0
alainvizzini@gmail.com <alainvizzini@gmail.com@a4e8eee6-8666-d384-cd2c-cc5a62ec2564>
2011-10-30 15:00:59 +0000

    First working version, problem with triple-clap not properly ignored.

--- trunk/Attiny2313_Clapclap/Attiny2313_Clapclap.c
+++ trunk/Attiny2313_Clapclap/Attiny2313_Clapclap.c
@@ -10,34 +10,132 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
+//#define F_CPU 8000000L //already in the makefile
+#define CYCLE_PER_MS (F_CPU / 1000L)
+
+//this mystery divider is obtained via direct measures. No idea why this value comes out, can't find a logical explanation (counting cycles of my code?).
+//it makes something like 13,333 cycles per second. I don't need precise measures, so it's ok, but still it's a shame...
+#define MYSTERY_DIVIDER_SEC (8000UL * 2UL * 50UL / 60UL)
+#define MYSTERY_DIVIDER_MILISEC (8UL * 2UL * 50UL / 60UL)
+
+#define CLAP_GAP_IN_MS 350
+#define CLAP_GAP_TOLERANCE 200
+
+#define CLAP_GAP_MIN (CLAP_GAP_IN_MS - CLAP_GAP_TOLERANCE)
+#define CLAP_GAP_MAX (CLAP_GAP_IN_MS + CLAP_GAP_TOLERANCE)
+
+
+volatile uint32_t mTimeCounterMSec = 0;
+volatile uint32_t mTimeLastClap = 0;
 
 //B3 is 0x08 = b00001000
-void test_blinkB3(){
-	PORTB ^= 0x08; //led on
-	_delay_ms(50);
-	PORTB ^= 0x08; // led off
+void blink_clap(){
+	PORTB ^= 0x08; //toggle led
+	PORTD ^= (1 << 4); //toggle led
+	_delay_ms(10);
+	PORTB ^= 0x08; // toggle led
+	PORTD ^= (1 << 4); //toggle led
 }
 
 
 //B2 is 0x04 = b00000100
-void test_blinkB2(){
-	PORTB ^= 0x04; //led on
-	_delay_ms(100);
-	PORTB ^= 0x04; // led off
+void blink_doubleClapDetected(){
+	PORTB ^= 0x04; //toggle led
+	PORTD ^= (1 << 5); //toggle led
+	_delay_ms(10);
+	PORTB ^= 0x04; // toggle led
+	PORTD ^= (1 << 5); //toggle led
 }
 
 
 //Interrupt code of the comparator
 // Our settings is interrupt on rising edge. Meaning no 2 consequent interrupt if stays high. It is rose when comparator output goes from 0 to 1 only.
-// NB: AIN0 is POSITIVE pin, AIN1 is NEGATIVE pin
+// NB  : AIN0 is POSITIVE pin, AIN1 is NEGATIVE pin
+// NB2 : you need to stabilize the power rail with a capacitor (otherwise you'll have ripples and misreading). With AIN1=Comp-=0v a 200uF is fine.
 ISR(ANA_COMP_vect) {
 
 	//with this test, ensure that indeed the interrupt result is 1 = V(AIN0) > V(AIN1)
 	//(double check ?)
 	if ( (ACSR & (1 << ACO)) != 0 ) {
-		test_blinkB2();	
+		blink_clap();	
+		
+		volatile uint32_t vNow = mTimeCounterMSec;
+		volatile uint32_t vGap = vNow - mTimeLastClap;
+		
+		if (
+			mTimeLastClap != 0UL
+			&& mTimeLastClap < vNow
+			&& vGap >= CLAP_GAP_MIN && vGap <= CLAP_GAP_MAX
+			) {
+		
+			blink_doubleClapDetected();
+			mTimeLastClap = 0UL;
+		}
+		else {
+			mTimeLastClap = vNow;
+		}
 	}
 
+}
+
+//Test purpose. This pulses 1 per second
+//Sorry, no idea why the math work here ... that puzzles me.
+void test_pulse1perSecond(){
+	uint32_t v = 0;
+	
+	while(1) {
+		v++;
+			
+		//do NOT ask me why with this formula ! get 1 pulse per sec, but it's a fact...
+		//the final 50/60 was made from direct measures (how many pulses on a minute)
+		if ((v % MYSTERY_DIVIDER_SEC) == 0L){
+		
+			blink_clap();
+			v=0;
+		}
+	}
+	
+	//this means 1 second is 13,333 cycles... look like the thing runs at 13 kHz oO; ??
+}
+
+//Test purpose. This pulses 1 per second
+//Sorry, no idea why the math work here ... that puzzles me.
+void test_pulse1perSecond2(){
+	uint32_t v = 0;
+	
+	while(1) {
+		v++;
+			
+		//do NOT ask me why with this formula ! get 1 pulse per sec, but it's a fact...
+		//the final 50/60 was made from direct measures (how many pulses on a minute)
+		if ((v % MYSTERY_DIVIDER_MILISEC) == 0L){
+			if ((v % 1000L) == 0L){
+				blink_clap();
+				v=0;
+			}
+		}
+	}
+	
+	//this means 1 second is 13,333 cycles... look like the thing runs at 13 kHz oO; ??
+}
+
+//Counts seconds
+void counterSeconds(){
+	volatile uint32_t v = 0UL;
+	
+	while(1) {
+		v++;
+			
+		//do NOT ask me why with this formula ! get 1 pulse per sec, but it's a fact...
+		//the final 50/60 was made from direct measures (how many pulses on a minute)
+		if ((v % MYSTERY_DIVIDER_MILISEC) == 0UL){
+			mTimeCounterMSec++;
+			
+			if ((v % 1000UL) == 0UL){
+				v=0UL;
+			}	
+		}
+	}
 }
 
 //
@@ -52,6 +150,8 @@ int main(void) {
 
 	//port B pins 7-2 go output, 1-0 go input(it's the comparator)
     DDRB = 0xFC;
+	//On the final stuff I'll use pins on row D, sol let's turn them output too
+	DDRD = 0xFF;
 	
 	//needed it seems. 
 	//One need also a pulldown on the AIN0 (Comparator+) to avoid having it floating (if you use a switch for instance)
@@ -71,10 +171,23 @@ int main(void) {
 	
 	sei();                 // enable global interrupts
   
-	while (1) {/*all the job is done in the interrupt */
+  /*
+	while (1) {//all the job is done in the interrupt 
 		// a little "is alive" debug trick that blink another led every second or so
-		test_blinkB3(); _delay_ms(1000);
+		//blink_clap(); _delay_ms(1000);
+		
+		mTimeCounterCycle++;
+		if (mTimeCounterCycle >= CYCLE_PER_MS){
+			//one millisec (or so) has passed
+			mTimeCounterMSec++;
+			mTimeCounterCycle = 0;
+		}
 	}
+	*/
+
+	//test_pulse1perSecond();
+	counterSeconds();
+	//test_pulse1perSecond2();
 }
 
 
