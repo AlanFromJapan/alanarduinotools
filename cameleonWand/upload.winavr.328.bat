7929f1385bcadd6525ccf71fff27e54851192e05
alainvizzini@gmail.com <alainvizzini@gmail.com@a4e8eee6-8666-d384-cd2c-cc5a62ec2564>
2011-12-26 15:16:26 +0000

    Renamed to make ArduinoIDE happy.

--- cameleonWand/upload.winavr.328.bat
+++ cameleonWand/upload.winavr.328.bat
@@ -1,2 +1,2 @@
-avrdude  -c avrispv2 -p m328p -P COM1 -U flash:w:C:\Users\Alan\AppData\Local\Temp\build5524655155233521615.tmp\cameleon.cpp.hex:i 
+avrdude  -c avrispv2 -p m328p -P COM1 -U flash:w:C:\Users\Alan\AppData\Local\Temp\build5524655155233521615.tmp\cameleonWand.cpp.hex:i 
 pause
