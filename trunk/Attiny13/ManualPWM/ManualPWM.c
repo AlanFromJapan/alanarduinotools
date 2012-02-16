b99c4c8e8018538d33c820ea3beb05df42871844
alainvizzini@gmail.com <alainvizzini@gmail.com@a4e8eee6-8666-d384-cd2c-cc5a62ec2564>
2012-02-16 13:58:19 +0000

    Analog read ok, show ok. Temp 2 RGB basic version (no fade).

--- trunk/Attiny13/ManualPWM/ManualPWM.c
+++ trunk/Attiny13/ManualPWM/ManualPWM.c
@@ -12,11 +12,49 @@
 
 uint8_t mRGB[3];
 
+
+uint8_t ReadADC() 
+{
+	ADCSRA |= (1 << ADEN); // Analog-Digital enable bit
+	ADCSRA |= (1 << ADSC); // Start conversion
+ 
+	while (ADCSRA & (1 << ADSC)); // wait until conversion is done
+ /*
+	ADCSRA |= (1 << ADSC); // start single conversion
+ 
+	while (ADCSRA & (1 << ADSC)) // wait until conversion is done
+ 
+	ADCSRA &= ~(1<<ADEN); // shut down the ADC */
+		 
+	return ADCH;
+}
+
+//Simple conversion by zone, no fading
+//inline saves 50 byte ... 50byte for 2 jump and stack in/stack out ?
+inline void Temp2RGB_Simple( uint8_t pTemp )	{
+	mRGB[0] = 0;
+	mRGB[1] = 0;
+	mRGB[2] = 0;
+			
+	if (pTemp <= 13){
+		//blue
+		mRGB[2] = 255;
+	}
+	else {
+		if (pTemp <= 20){
+			mRGB[1] = 255;
+		}
+		else {
+			mRGB[0] = 255;
+		}
+	}
+}
+
 int main(void)
 {
-	mRGB[0] = 150;
-	mRGB[1] = 255;
-	mRGB[2] = 255;
+	mRGB[0] = 0;
+	mRGB[1] = 0;
+	mRGB[2] = 0;
 
 	
 	//Timer full speed !
@@ -26,17 +64,57 @@ int main(void)
 	//port B pins 0-5 go OUTPUT
     DDRB = 0xFF;
 	
+	
+	//------------START ADC INIT --------------------------
+	 ADCSRA |= (1 << ADEN);
+ /*| // Analog-Digital enable bit
+ (1 << ADPS1)| // set prescaler to 8 (clock / 8)
+ (1 << ADPS0); // set prescaler to 8 (clock / 8)
+ */
+ ADMUX |= 
+	(1 << ADLAR) // AD result store in (more significant bit in ADCH) : ADCH holds the 8 MSB like that: perfect for PWM
+	| (1 << MUX1); // Choose AD input AD2 (BP 4)
+	//------------END ADC INIT --------------------------
+	
+	
+	uint8_t vADCCheckRound = 0;
 	uint8_t vRGBCurrentRound = 0;
     while(1)
     {
+		
+		if(vADCCheckRound == 0){
+			uint8_t vADCValue = ReadADC();
+			
+			//uint8_t vTemp = (uint8_t)((uint32_t)vADCValue * 5UL * 100UL / 255UL );
+			//replace a /255 by a shift right 8 bit and save 92 bytes! 10% of the memory!!! 
+			//WTF a divide costs 92 bytes ? Oo;
+			uint8_t vTemp = (uint8_t)(((uint32_t)vADCValue * 5UL * 100UL) >> 8UL);	
+			
+			Temp2RGB_Simple(vTemp);
+
+		}
+		
+		
+		
         //todo : put this in an interrupt
 		if (vRGBCurrentRound == 0){
+			
 			//make pins B0-2 high
 			PORTB = 
 				(1 << PORTB0) 
 				| (1 << PORTB1)
 				| (1 << PORTB2)
 				;
+				
+			/*
+			PORTB = 0x00;
+			if (mRGB[0] > 0)
+				PORTB |= (1 << PORTB0);
+			if (mRGB[1] > 0)
+				PORTB |= (1 << PORTB1);
+			if (mRGB[2] > 0)
+				PORTB |= (1 << PORTB2);
+				*/
 		}
 		
 
@@ -57,5 +135,7 @@ int main(void)
 		//and the POV
 		_delay_us(50);
 		vRGBCurrentRound++;
+		
+		vADCCheckRound++;
     }
 }
