96b2f9b4b03503eaa7c35f62c3b5ea7eb8d94712
alainvizzini@gmail.com <alainvizzini@gmail.com@a4e8eee6-8666-d384-cd2c-cc5a62ec2564>
2012-07-27 14:58:04 +0000

    Work version, not working at all uunfortunately, using only one interrupt ... let's start again but keep this for memory.

--- trunk/Attiny2313/AttinySNESPad/AttinySNESPad.c
+++ trunk/Attiny2313/AttinySNESPad/AttinySNESPad.c
@@ -52,57 +52,156 @@
 
 volatile uint16_t mCountTest = 0;
 
-uint16_t mButtonStatus = (1 << ORDER_BUTTON_START);
+volatile uint16_t mButtonStatus = 0xFFFF; // all UP (nothing pressed)
 
+volatile uint8_t mTestFlag = 0;
 
-void SendButtonStatus2() 
+void SendButtonStatusTEST2() 
 {
-			
-PORTB = 0xFF;
-PORTA = 0xFF;
+	PORTA = 0xFF;
 
-if (mCountTest % 180 == 0) {
+	if (mCountTest == 600){
+		mButtonStatus = (1 << ORDER_BUTTON_START);
+		
+		for (uint8_t i = 0; i < 16; i++){
+			if ((mButtonStatus & (1 << i)) == 0){
+				PORTA = 0x00;
+			}
+			else {
+				PORTA = 0xFF;
+			}
+			_delay_loop_1(12); 
+		}
+	}	
+	
+	if (mCountTest > 600 && mCountTest % 120 == 0){
+		mTestFlag++;
+		//if (mTestFlag % 2 == 0){
+			//mButtonStatus = (1 << ORDER_BUTTON_RIGHT);
+		//}	
+		//if (mTestFlag % 2 == 1){
+			//mButtonStatus = (1 << ORDER_BUTTON_DOWN);
+		//}
+		
+		//mButtonStatus = ~mButtonStatus;
+		
+		//mButtonStatus = ~(1 << (mTestFlag % 16));
+		
+		_delay_loop_1(1); 
+		if (mTestFlag % 2 == 0){
+			mButtonStatus = ~(1 << ORDER_BUTTON_LEFT);
+		}
+		else {
+			mButtonStatus = ~(1 << ORDER_BUTTON_RIGHT);
+		}
+		
+		for (uint8_t i = 0; i < 16; i++){
+			if ((mButtonStatus & (1 << i)) == 0){
+				PORTA = 0x00;
+			}
+			else {
+				PORTA = 0xFF;
+			}
+			_delay_loop_1(12); 
+		}
+	}	
+		
+	PORTA = 0xFF;	
+
+	mCountTest++;
+}
+
+void SendButtonStatusTEST() 
+{
+	PORTA = 0xFF;
+
+	if (mCountTest % 180 == 0){
+		mButtonStatus = (1 << ORDER_BUTTON_START);
+		
+		for (uint8_t i = 0; i < 16; i++){
+			if ((mButtonStatus & (1 << i)) == 0){
+				PORTA = 0x00;
+			}
+			else {
+				PORTA = 0xFF;
+			}
+			_delay_loop_1(12); 
+		}
+	}	
+		
+	PORTA = 0xFF;	
+
+	mCountTest++;
+}
+
+void SendButtonStatusTESTAAAA() 
+{
+	PORTA = 0xFF;
+
+	
+		mButtonStatus = 0xAAAA;
+		
+		for (uint8_t i = 0; i < 16; i++){
+			if ((mButtonStatus & (1 << i)) == 0){
+				PORTA = 0x00;
+			}
+			else {
+				PORTA = 0xFF;
+			}
+			_delay_loop_1(12); 
+		}
+	
+		
+	PORTA = 0xFF;	
+
+	mCountTest++;
+}
+
+void SendButtonStatus() 
+{
+	PORTA = 0xFF;
+
 	for (uint8_t i = 0; i < 16; i++){
 		if ((mButtonStatus & (1 << i)) == 0){
 			PORTA = 0x00;
-			PORTB = 0x00;
 		}
 		else {
 			PORTA = 0xFF;
-			PORTB = 0xFF;
 		}
-		_delay_loop_1(12);
+		_delay_loop_1(12); 
 	}
-}
-PORTB = 0xFF;	
-PORTA = 0xFF;	
+		
+	PORTA = 0xFF;	
 
-mCountTest++;
+	mCountTest++;
 }
 
+//Read the buttons status and store it into the global variable mButtonStatus
+void ReadButtonsStatus() 
+{
+	mButtonStatus = 0xFFFF; // all UP (nothing pressed)
+	
+	if ((PORTB & (1 << 0)) == 0) {
+		//button pressed
+		mButtonStatus ^= (1 << ORDER_BUTTON_START);
+	}
+	
+	mButtonStatus = ~mButtonStatus;
+}
 
 //Handles the INT1 pin : LATCH signal going DOWN (falling hedge)
 SIGNAL (SIG_INT1)
 { 
-	//cli();
+	SendButtonStatusTEST2();
 	
-    // Do some stuff
-	//PORTB = 0xFF;
-/*
-	//blink
-	PORTD ^= (1 << 0);
-	_delay_ms(1000);
-	PORTD ^= (1 << 0);
-*/
+	//Yes, send the previous then read. Because we want to start on time so reading now would delay us.
+	//Plus we read at 60Hz so there's a very low chance that it makes any noticeable difference for the player (really)
+	
+	//Send the previously memorized buttons status
+	//SendButtonStatus();
 
-
-	SendButtonStatus2();
-	
-	//mCountTest++;
-	
-	//PORTB = 0x00;
-	
-	//sei();
+	//Read the new status(take your time, next interrupt is in 16ms...)
+	//ReadButtonsStatus();
 }
 
 void setupInterrupt(){
@@ -135,8 +234,8 @@ int main(void)
 	//port d as output except the int1 (PD3)
 	DDRD = 0xFF ^ (1 << PD3);
 	
-	//for the test
-	//DDRB = 0xFF;
+	//port b is only inputs (the buttons)
+	DDRB = 0x00;
 	
 	//turn on the interrupts
 	setupInterrupt();
@@ -144,12 +243,6 @@ int main(void)
 	
     while(1)
     {
-		/*
-		//loop
-		PORTD ^= (1 << 1);
-		_delay_ms(300);
-		PORTD ^= (1 << 1);
-		_delay_ms(1200);
-		*/
+		//all the job is done inn the interrupt, just loop 'till the end of time
     }
 }
