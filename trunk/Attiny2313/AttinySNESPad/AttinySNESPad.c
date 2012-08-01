0bf7eb6d376937a28fd75c37ddbdbb16b0f75bdc
alainvizzini@gmail.com <alainvizzini@gmail.com@a4e8eee6-8666-d384-cd2c-cc5a62ec2564>
2012-08-01 15:16:48 +0000

    Working! now can simulate nicely start and right. A little refactor, prepare the pin reading and soldering the buttons and it's a go !

--- trunk/Attiny2313/AttinySNESPad/AttinySNESPad.c
+++ trunk/Attiny2313/AttinySNESPad/AttinySNESPad.c
@@ -52,155 +52,10 @@
 #define PIN_CLOCK				0
 #define PIN_DATA				1
 
-volatile uint16_t mCountTest = 0;
+volatile uint8_t mCurrentButtonIndex = 0;
 
 volatile uint16_t mButtonStatus = 0xFFFF; // all UP (nothing pressed)
 
-volatile uint8_t mTestFlag = 0;
-
-void SendButtonStatusTEST2() 
-{
-	PORTA = 0xFF;
-
-	if (mCountTest == 600){
-		mButtonStatus = (1 << ORDER_BUTTON_START);
-		
-		for (uint8_t i = 0; i < 16; i++){
-			if ((mButtonStatus & (1 << i)) == 0){
-				PORTA = 0x00;
-			}
-			else {
-				PORTA = 0xFF;
-			}
-			_delay_loop_1(12); 
-		}
-	}	
-	
-	if (mCountTest > 600 && mCountTest % 120 == 0){
-		mTestFlag++;
-		//if (mTestFlag % 2 == 0){
-			//mButtonStatus = (1 << ORDER_BUTTON_RIGHT);
-		//}	
-		//if (mTestFlag % 2 == 1){
-			//mButtonStatus = (1 << ORDER_BUTTON_DOWN);
-		//}
-		
-		//mButtonStatus = ~mButtonStatus;
-		
-		//mButtonStatus = ~(1 << (mTestFlag % 16));
-		
-		_delay_loop_1(1); 
-		if (mTestFlag % 2 == 0){
-			mButtonStatus = ~(1 << ORDER_BUTTON_LEFT);
-		}
-		else {
-			mButtonStatus = ~(1 << ORDER_BUTTON_RIGHT);
-		}
-		
-		for (uint8_t i = 0; i < 16; i++){
-			if ((mButtonStatus & (1 << i)) == 0){
-				PORTA = 0x00;
-			}
-			else {
-				PORTA = 0xFF;
-			}
-			_delay_loop_1(12); 
-		}
-	}	
-		
-	PORTA = 0xFF;	
-
-	mCountTest++;
-}
-
-void SendButtonStatusTEST() 
-{
-	PORTA = 0xFF;
-
-	if (mCountTest % 180 == 0){
-		mButtonStatus = (1 << ORDER_BUTTON_START);
-		
-		for (uint8_t i = 0; i < 16; i++){
-			if ((mButtonStatus & (1 << i)) == 0){
-				PORTA = 0x00;
-			}
-			else {
-				PORTA = 0xFF;
-			}
-			_delay_loop_1(12); 
-		}
-	}	
-		
-	PORTA = 0xFF;	
-
-	mCountTest++;
-}
-
-void SendButtonStatusTESTAAAA() 
-{
-	PORTA = 0xFF;
-
-	
-		mButtonStatus = 0xAAAA;
-		
-		for (uint8_t i = 0; i < 16; i++){
-			if ((mButtonStatus & (1 << i)) == 0){
-				PORTA = 0x00;
-			}
-			else {
-				PORTA = 0xFF;
-			}
-			_delay_loop_1(12); 
-		}
-	
-		
-	PORTA = 0xFF;	
-
-	mCountTest++;
-}
-
-void SendButtonStatus() 
-{
-	PORTA = 0xFF;
-
-	for (uint8_t i = 0; i < 16; i++){
-		if ((mButtonStatus & (1 << i)) == 0){
-			PORTA = 0x00;
-		}
-		else {
-			PORTA = 0xFF;
-		}
-		_delay_loop_1(12); 
-	}
-		
-	PORTA = 0xFF;	
-
-	mCountTest++;
-}
-
-//Read the buttons status and store it into the global variable mButtonStatus
-void ReadButtonsStatus() 
-{
-	mButtonStatus = 0xFFFF; // all UP (nothing pressed)
-	
-	if ((PORTB & (1 << 0)) == 0) {
-		//button pressed
-		mButtonStatus ^= (1 << ORDER_BUTTON_START);
-	}
-	
-	mButtonStatus = ~mButtonStatus;
-}
-
-
-void ReadButtonsStatus_TESTReadSetDown() 
-{
-	mButtonStatus = 0xFFFF; // all UP (nothing pressed)
-	
-	if ((PINB & (1 << 0)) == 0) {
-		//button pressed
-		mButtonStatus = 0x0000;
-	}
-}
 
 void ReadButtonsStatus_TESTReadSetDown2() 
 {
@@ -208,92 +63,63 @@ void ReadButtonsStatus_TESTReadSetDown2()
 	
 	if ((PINB & (1 << 1)) == 0) {
 		//button pressed
-		mButtonStatus = 0xFFFF ^ (1 << ORDER_BUTTON_START);
-	}
-}
-
-//Fake, return starts
-void ReadButtonsStatus_TestSTART() 
-{
-	mButtonStatus = 0x0000; // all UP (nothing pressed)
-	
 		mButtonStatus ^= (1 << ORDER_BUTTON_START);
+	}
 	
-	mButtonStatus = ~mButtonStatus;
+	if ((PINB & (1 << 2)) == 0) {
+		//button pressed. DONT FORGET to force the stupid compiler to work with uint16!!
+		mButtonStatus ^= (uint16_t)(1 << ORDER_BUTTON_RIGHT);
+	}
+
 }
 
-//fake return 0x0000
-void ReadButtonsStatus_TestAllDown() 
-{
-	mButtonStatus = 0x0000; // all down
-}
-
-//fake return 0b1010101010
-void ReadButtonsStatus_TestAlternate() 
-{
-	mButtonStatus = 0b1010101010101010; // all down
-}
-
-//Fake, return starts once in a while
-void ReadButtonsStatus_TestSTART2() 
-{
-	if (mTestFlag == 0 ){
-	
-	
-		//all down
-		mButtonStatus = 0x0000;
-	
+//Write one value on the DATA line
+//inline : in short the compiler will copy the code and not make a jump to function, to go faster.
+//no pointer, short, need of extra speed : the perfect client for /inline/.
+inline void WriteNextData(){
+	//push the first value on the clock (will be read on CLOCK going DOWN)
+	if ((mButtonStatus & (1 << mCurrentButtonIndex)) == 0){
+		PORTA = 0x00;
 	}
 	else {
-		//all up
-		mButtonStatus = 0xFFFF;
-	}	
+		PORTA = 0xFF;
+	}
 	
-	mTestFlag++;
+	//go next
+	mCurrentButtonIndex++;
 }
 
-
-
-//Handles the INT1 pin : LATCH signal going DOWN (falling hedge) : STARTUP
+//Handles the INT1 pin : LATCH signal going DOWN (falling hedge) : STARTUP 
 SIGNAL (SIG_INT1)
 { 
 	//read button status before the clock ticks in
 	ReadButtonsStatus_TESTReadSetDown2();
-	mCountTest = 0;
+	mCurrentButtonIndex = 0;
 	
-	if ((mButtonStatus & (1 << mCountTest)) == 0){
-		PORTA = 0x00;
-	}
-	else {
-		PORTA = 0xFF;
-	}
+	//put already the first bit on the rail for reading 
+	WriteNextData();
 }
 
-//Handles the INT0 pin : LATCH signal going DOWN (falling hedge) : CLOCK
+//Handles the INT0 pin : CLOCK signal going DOWN (falling hedge) = reading
+// push next value to the value pin
 SIGNAL (SIG_INT0)
 { 
-	
-	if ((mButtonStatus & (1 << mCountTest)) == 0){
-		PORTA = 0x00;
-	}
-	else {
-		PORTA = 0xFF;
-	}
-		
-	mCountTest++;
+	//move next
+	WriteNextData();
 }
 
 void setupInterrupt(){
 	//use INT1 and INT0
 	PCMSK |= (1 << PIND3) | (1 << PIND2);
 	
-	// interrupt on INT0/INT1 pin falling edge (sensor triggered) 
-	MCUCR |= (1<<ISC11) | (1<<ISC01);
+	// interrupt on INT1 pin falling edge 
+	//INT0 goes on falling hedge
+	MCUCR |= (1<<ISC11) | (1<<ISC01); // | (1<<ISC00);
 
 	// turn on interrupts!
 	GIMSK  |= (1<<INT1) | (1<<INT0);
 	
-	//without a call to sei() it doesn't work it seems...
+	//without a call to sei() it doesn't work 
 	sei();
 }
 
