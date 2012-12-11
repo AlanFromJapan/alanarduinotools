8f7cbe969ea24a7391f5149920b861cd9f1518b7
alainvizzini@gmail.com <alainvizzini@gmail.com@a4e8eee6-8666-d384-cd2c-cc5a62ec2564>
2012-12-11 15:14:48 +0000

    Now displays raw 10bits value read on ADC5 (PC5)

--- trunk/Atmegaxx8/WhatAWatt/WhatAWatt.cpp
+++ trunk/Atmegaxx8/WhatAWatt/WhatAWatt.cpp
@@ -11,15 +11,50 @@
 #include <avr/io.h>
 #include "Rapide7seg.h"
 
+
+//Read ADC value into a 16 bits uint16
+uint16_t ReadADCx16()
+{	
+	//stop all digit display (to avoid noise)
+	PORTD = 0xff;
+	
+	ADCSRA |= (1 << ADSC); // Start conversion
+	while (ADCSRA & (1 << ADSC)); // wait until conversion is done
+
+	uint16_t v = ADCL;
+	v = (ADCH << 8) + v;
+
+	
+	return v;
+}
+
+void InitADC(){
+	/*********************************************************************/
+	//ADC setup
+	//ADC5, right adjust, using aref
+	//ADMUX = 0x00;
+	ADMUX = 0x05 | (1 << REFS0) | (0 << ADLAR);
+	
+	ADCSRA |= (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADEN);
+	
+	/*********************************************************************/
+	
+}
+
 int main(void)
 {
 	setup7seg();
+	InitADC(); 
 	
     while(1)
     {
+		/*
 		for (uint16_t vNumber = 0; vNumber < 1000; vNumber++){
 			uint16_t vVal = vNumber * 10;
 			showNumber(vVal, 0, 2);
 		}
+		*/
+		
+		showNumber(ReadADCx16() *10, 0, 2);
     }
 }
