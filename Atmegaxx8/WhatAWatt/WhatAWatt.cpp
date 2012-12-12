744a33c9149ddab4bc80a110225ef742c4262bab
alainvizzini@gmail.com <alainvizzini@gmail.com@a4e8eee6-8666-d384-cd2c-cc5a62ec2564>
2012-12-12 15:53:17 +0000

    Now shows Watt consumption. Need to check the formula (shall not use effective current?) Need to find something for the display (interrupt?) to remove the annoying blinking.

--- Atmegaxx8/WhatAWatt/WhatAWatt.cpp
+++ Atmegaxx8/WhatAWatt/WhatAWatt.cpp
@@ -41,10 +41,16 @@ void InitADC(){
 	
 }
 
+#define AVERAGE_MAX 5
+
 int main(void)
 {
 	setup7seg();
 	InitADC(); 
+	
+	uint16_t vAverageTab[AVERAGE_MAX] ;
+	uint8_t vAverageCount = 0;
+	uint8_t vAverageIndex = 0;
 	
     while(1)
     {
@@ -55,6 +61,33 @@ int main(void)
 		}
 		*/
 		
-		showNumber(ReadADCx16() *10, 0, 2);
+		//showNumber(ReadADCx16() *10, 0, 2);
+		
+		//get a value, read many times to normally read a MAX local (the top of the AC sine)
+		uint16_t vMaxCurrent = 0;
+		for (uint16_t i = 0; i < 200; i++) {
+			uint16_t vValue = ReadADCx16();
+			vMaxCurrent = (vValue > vMaxCurrent ? vValue : vMaxCurrent);
+			_delay_ms(2);
+		}	
+		
+		vAverageTab[vAverageIndex] = vMaxCurrent;
+		vAverageIndex = (vAverageIndex + 1) % AVERAGE_MAX; //tab length 
+		vAverageCount = (1+vAverageCount >AVERAGE_MAX ?AVERAGE_MAX : 1+vAverageCount);
+		
+		float vAvgCurrent = 0;
+		for(uint8_t i = 0; i < vAverageCount; i++){
+			vAvgCurrent += vAverageTab[i];
+		}
+		vAvgCurrent = vAvgCurrent / (float)AVERAGE_MAX;
+		
+		//current and effective current are in mA
+		float current=vAvgCurrent/1024.0*5.0/800.0*2000000.0;
+		float effective_value=current/1.414;
+		
+		//Japan is 100 V, Watts = mA/ 1000 x 100V = mA /10
+		//BUT I use only 3 x 7 segs aligned to the left, so I need to shift the value 1 digit to the left => x10
+		//CCL : Watts = value mA /10 x 10 = value mA
+		showNumber((uint16_t)(effective_value), 0, 2);		
     }
 }
