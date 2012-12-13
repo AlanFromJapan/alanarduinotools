a7080df934f65c0adc943d05f867e8fc1d084d31
alainvizzini@gmail.com <alainvizzini@gmail.com@a4e8eee6-8666-d384-cd2c-cc5a62ec2564>
2012-12-13 14:27:14 +0000

    New version with interrupts : display is smooth, no blinking. Results look roughly ok, maybe a bit too volatile.

--- trunk/Atmegaxx8/WhatAWatt/WhatAWatt.cpp
+++ trunk/Atmegaxx8/WhatAWatt/WhatAWatt.cpp
@@ -9,6 +9,7 @@
 #define F_CPU 8000000
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include "Rapide7seg.h"
 
 
@@ -41,7 +42,73 @@ void InitADC(){
 	
 }
 
-#define AVERAGE_MAX 5
+//Setup the timer to read in the background
+void setupTimer0(){
+	// Prescaler = FCPU/64
+	// 8MHz / 64 / 256bits => overflow about every 2ms
+	//TCCR0B |= (1<<CS01)|(1<<CS00);
+
+	// Prescaler = FCPU/256
+	// 8MHz / 256 / 256bits => overflow about every 8ms
+	TCCR0B |= (1<<CS02);
+
+	
+	//overflow of timer 0
+	TIMSK0 |= (1 << TOIE0);
+	
+	//reset counter to zero
+	TCNT0 = 0x00;
+}
+
+//number of average values
+#define AVERAGE_MAX 15
+#define OVERFLOW_COUNT 20
+#define OVERFLOW_COUNT_NEW_AVG (AVERAGE_MAX * OVERFLOW_COUNT)
+volatile uint16_t	mAverageTab[AVERAGE_MAX] ;
+volatile uint8_t	mAverageIndex = 0;
+volatile uint16_t	mMaxVoltage = 0;
+volatile uint16_t	mOverflowCount = 0;
+volatile float		mLatestPower = 0;
+	
+//Timer 0 overflow interrupt
+//Triggered about 488 times a sec
+ISR(TIMER0_OVF_vect){
+	uint16_t vValue = ReadADCx16();
+	mMaxVoltage = (vValue > mMaxVoltage ? vValue : mMaxVoltage);
+	
+	mOverflowCount++;
+	
+	//more than 20 times => scanned max for 20 x 2ms = 40ms (120Hz)
+	if (mOverflowCount >= OVERFLOW_COUNT){
+		//get a value, read many times to normally read a MAX local (the top of the AC sine)
+		mAverageTab[mAverageIndex] = mMaxVoltage;
+		mAverageIndex = (mAverageIndex + 1) % AVERAGE_MAX; //tab length	
+		
+		//vMaxCurrent= 0;
+	}
+	
+	//got 5 readings, update the average to display
+	if (mOverflowCount >= OVERFLOW_COUNT_NEW_AVG){
+		float vAvgVoltage = 0;
+		for(uint8_t i = 0; i < AVERAGE_MAX; i++){
+			vAvgVoltage += mAverageTab[i];
+		}
+		vAvgVoltage = vAvgVoltage / (float)AVERAGE_MAX;
+				
+		//current and effective current are in mA
+		float current=vAvgVoltage/1024.0*5.0/800.0*2000000.0;
+		float effective_value=current/1.414;
+		
+		//Japan is 100 V, Watts = mA/ 1000 x 100V = mA /10
+		//BUT I use only 3 x 7 segs aligned to the left, so I need to shift the value 1 digit to the left => x10
+		//CCL : Watts = value mA /10 x 10 = value mA
+		mLatestPower = effective_value;
+		
+		mOverflowCount = 0;
+		mMaxVoltage= 0;
+	}
+}
+
 
 int main(void)
 {
@@ -52,47 +119,15 @@ int main(void)
 		
 	setup7seg();
 	InitADC(); 
+	setupTimer0();
 	
-	uint16_t vAverageTab[AVERAGE_MAX] ;
-	uint8_t vAverageCount = 0;
-	uint8_t vAverageIndex = 0;
+	//start the interrupts
+	sei();
 	
     while(1)
     {
-		/*
-		for (uint16_t vNumber = 0; vNumber < 1000; vNumber++){
-			uint16_t vVal = vNumber * 10;
-			showNumber(vVal, 0, 2);
-		}
-		*/
-		
-		//showNumber(ReadADCx16() *10, 0, 2);
-		
-		//get a value, read many times to normally read a MAX local (the top of the AC sine)
-		uint16_t vMaxCurrent = 0;
-		for (uint16_t i = 0; i < 200; i++) {
-			uint16_t vValue = ReadADCx16();
-			vMaxCurrent = (vValue > vMaxCurrent ? vValue : vMaxCurrent);
-			_delay_ms(2);
-		}	
-		
-		vAverageTab[vAverageIndex] = vMaxCurrent;
-		vAverageIndex = (vAverageIndex + 1) % AVERAGE_MAX; //tab length 
-		vAverageCount = (1+vAverageCount >AVERAGE_MAX ?AVERAGE_MAX : 1+vAverageCount);
-		
-		float vAvgCurrent = 0;
-		for(uint8_t i = 0; i < vAverageCount; i++){
-			vAvgCurrent += vAverageTab[i];
-		}
-		vAvgCurrent = vAvgCurrent / (float)AVERAGE_MAX;
-		
-		//current and effective current are in mA
-		float current=vAvgCurrent/1024.0*5.0/800.0*2000000.0;
-		float effective_value=current/1.414;
-		
-		//Japan is 100 V, Watts = mA/ 1000 x 100V = mA /10
-		//BUT I use only 3 x 7 segs aligned to the left, so I need to shift the value 1 digit to the left => x10
-		//CCL : Watts = value mA /10 x 10 = value mA
-		showNumber((uint16_t)(effective_value), 0, 2);		
+		showNumber((uint16_t)(mLatestPower), 0, 2, false);		
     }
 }
+
+
