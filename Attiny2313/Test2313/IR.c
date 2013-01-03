0f4d6811e6461469209992abc45ece446ee8f4ea
alainvizzini@gmail.com <alainvizzini@gmail.com@a4e8eee6-8666-d384-cd2c-cc5a62ec2564>
2013-01-03 14:51:34 +0000

    ADded IR support for Sharp TV : working fine. Need to add NEC for the VCR.

---
+++ Attiny2313/Test2313/IR.c
@@ -0,0 +1,29 @@
+//#include "IR/IR_NEC.h"
+#include "IR/IR_Sharp.h"
+#include "IR/IR_NEC.h"
+
+void TestVCROnOff() {
+	//VCR is a Buffalo something and using a NEC extended format
+	//POWER 30EEB14E
+	//2 first bytes are the unique ID, the two last bytes are the command and it's binary opposite
+	
+	//transmit_nec_ir_commandExt(0x30EE, 0xB1);
+	
+	
+
+}	
+
+void TestTVOnOff(){
+		    //TV power, works ok
+		    //IRSendSharp (0x41A2, 15);
+			
+			//vol- ok
+			//IRSendSharp (0x42A2, 15);
+			
+			//vol+ ok
+			//IRSendSharp (0x40A2, 15);
+			
+			//input change ok
+			//IRSendSharp (0x4322, 15);
+	
+}
--- Attiny2313/Test2313/Test2313.c
+++ Attiny2313/Test2313/Test2313.c
@@ -14,12 +14,22 @@ int main(void)
 {
 	//TestPullups();
 	
-	AquestLSI_Init();
-	_delay_ms(1000);
+	//AquestLSI_Init();
+	//_delay_ms(1000);
+	
+	DDRD = 0xFF;
+	PORTD = 0xFF;
 	
     while(1)
     {
 		//SayKonnichiwa();
-	    SayAll();
+	    //SayAll();
+		
+		_delay_ms(3000);
+		//TestVCROnOff();
+		//PORTD = ~PORTD;
+		
+		
+		TestTVOnOff();
     }
 }
