aff4c67de8044adaa6f207ab98672c54d4df84b6
alainvizzini@gmail.com <alainvizzini@gmail.com@a4e8eee6-8666-d384-cd2c-cc5a62ec2564>
2013-02-17 13:38:06 +0000

    CHANGED the rgb temp led color (ManualPWM project) ADDED project for Dr Vetinari clock

---
+++ Attiny13/DrVetinaryClock/DrVetinaryClock.c
@@ -0,0 +1,39 @@
+/*
+ * DrVetinaryClock.c
+ *
+ * Created: 2013/02/16 18:11:19
+ *  Author: Alan
+ */ 
+
+//#define F_CPU 9600000
+
+#include <avr/io.h>
+#include <avr/delay.h>
+
+int main(void)
+{
+	//default uses the 9.6MHz internal crystal ... don't need so much speed	
+	//CKSEL |= 0x03; //internal oscillator
+	
+	CLKPR = (0<<CLKPCE); // & (1 << CLKPS3);
+	
+	
+	//all ports output!
+	DDRB = 0xFF;
+	
+	uint8_t vPort = (1 << PB4) | (0 << PB3);
+	
+	while (1){
+		_delay_ms(1000);
+		
+		PORTB |= (1 << PB4) | (1 << PB3);
+		_delay_ms(15);
+		PORTB &= ~((1 << PB4) | (1 << PB3));
+
+		_delay_ms(5);
+		PORTB |= (1 << PB4) | (1 << PB3);
+		_delay_ms(15);
+		PORTB &= ~((1 << PB4) | (1 << PB3));
+				
+	}
+}
