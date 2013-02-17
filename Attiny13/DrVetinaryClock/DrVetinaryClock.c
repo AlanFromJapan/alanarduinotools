aa1bb513d304a1348bf4cae9ccadf95e231c3fee
alainvizzini@gmail.com <alainvizzini@gmail.com@a4e8eee6-8666-d384-cd2c-cc5a62ec2564>
2013-02-17 15:21:03 +0000

    Does not skip seconds all the time but only time to time.

--- Attiny13/DrVetinaryClock/DrVetinaryClock.c
+++ Attiny13/DrVetinaryClock/DrVetinaryClock.c
@@ -1,19 +1,23 @@
 /*
- * DrVetinaryClock.c
- *
- * Created: 2013/02/16 18:11:19
- *  Author: Alan
- */ 
+* DrVetinaryClock.c
+*
+* Created: 2013/02/16 18:11:19
+*  Author: Alan
+*/
 
 //#define F_CPU 9600000
 
 #include <avr/io.h>
 #include <avr/delay.h>
 
-#define MAX_RAND 3
-
+//maximum number of seconds to skip
+#define MAX_RAND 5
+//time to sleep between each tick
+#define TICK_SLEEP_MS 200
 //Approximate duration of 1 step to move 1 tick
-#define TICK_DURATION_MS 220
+#define TICK_DURATION_MS (TICK_SLEEP_MS + 20)
+//probability to skip time
+#define SKIP_EVERY_N 6
 
 volatile uint8_t vPort = (1 << PB4) | (0 << PB3);
 
@@ -26,10 +30,10 @@ inline void tick(){
 	vPort ^= (1 << PB4) | (1 << PB3);
 	
 }
-	
+
 int main(void)
 {
-	//default uses the 9.6MHz internal crystal ... don't need so much speed	
+	//default uses the 9.6MHz internal crystal ... don't need so much speed
 	//CKSEL |= 0x03; //internal oscillator
 	
 	CLKPR = (0<<CLKPCE); // & (1 << CLKPS3);
@@ -38,21 +42,30 @@ int main(void)
 	DDRB = 0xFF;
 	
 	while (1){
-		//How many seconds to sleep
-		uint8_t vR = 1 + rand() % MAX_RAND;
+		uint8_t vSkip = rand() % SKIP_EVERY_N;
+		
+		//if 0, do the time skip stuff, otherwise normal tick
+		if (vSkip == 0x00) {
+			//How many seconds to sleep
+			uint8_t vR = 1 + rand() % MAX_RAND;
 
-		//wait for x additional seconds
-		for (uint8_t i = 0; i < vR; i++){		
-			_delay_ms(1000 - TICK_DURATION_MS);
+			//wait for x additional seconds
+			for (uint8_t i = 0; i < vR; i++){
+				_delay_ms(1000 - TICK_DURATION_MS);
+				
+			}
 			
-		}		
-				
-		//make the clock move the missing movements
-		//One round duration is about MOVE_DURATION_MS
-		for (uint8_t i = 0; i < vR; i++){
+			//make the clock move the missing movements
+			//One round duration is about MOVE_DURATION_MS
+			for (uint8_t i = 0; i < vR; i++){
+				tick();
+				_delay_ms(TICK_SLEEP_MS);
+			}
+		}
+		else {
+			//normal case, just normal tick
+			_delay_ms(1000);
 			tick();
-			_delay_ms(200);			
-		}	
-				
+		}
 	}
 }
