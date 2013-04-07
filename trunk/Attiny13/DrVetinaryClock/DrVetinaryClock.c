abad0b52e84b5b60d2dd3a2138c0c7edfa0e29c6
alainvizzini@gmail.com <alainvizzini@gmail.com@a4e8eee6-8666-d384-cd2c-cc5a62ec2564>
2013-04-07 06:55:44 +0000

    Some adjustments for the timing : 1 sec is not 1000ms. Crystal problem ? Code problem?

--- trunk/Attiny13/DrVetinaryClock/DrVetinaryClock.c
+++ trunk/Attiny13/DrVetinaryClock/DrVetinaryClock.c
@@ -5,19 +5,23 @@
 *  Author: Alan
 */
 
-#define F_CPU 9600000
+//4.8Mhz internal with a 8 factor divisor 600kHz : save power and run until lower voltage
+#define F_CPU 600000
 
 #include <avr/io.h>
 #include <avr/delay.h>
 
 //maximum number of seconds to skip
-#define MAX_RAND 5
+#define MAX_RAND 4
 //time to sleep between each tick
-#define TICK_SLEEP_MS 200
+#define TICK_SLEEP_MS 180
 //Approximate duration of 1 step to move 1 tick
 #define TICK_DURATION_MS (TICK_SLEEP_MS + 20)
 //probability to skip time
 #define SKIP_EVERY_N 45
+
+//after multiple test, one second shall not be 1,000ms but a little less. This works for me.
+#define ONE_SECOND_MS (925)
 
 //duration of the pulse according the voltage
 #define TICK_PULSE_MS_5V 7
@@ -27,7 +31,7 @@ volatile uint8_t mPort = (1 << PB4) | (0 << PB3);
 
 inline void tick(){
 	PORTB = mPort;
-	_delay_ms(TICK_PULSE_MS_5V);
+	_delay_ms(TICK_PULSE_MS_3V);
 	PORTB = 0x00;
 
 	//reverse status of PB3 and PB4 : the electro-magnet must change direction each time
@@ -37,13 +41,14 @@ inline void tick(){
 
 int main(void)
 {
-	//default uses the 9.6MHz internal crystal ... and leave it as is.
+	//no diviser : use the 4.8MHz / 8 CLKDIV8 fuse = 600kHz
 	CLKPR = (0<<CLKPCE); 
 	
 	//all ports output!
 	DDRB = 0xFF;
 	
 	while (1){
+
 		uint8_t vSkip = rand() % SKIP_EVERY_N;
 		
 		//if 0, do the time skip stuff, otherwise normal tick
@@ -53,7 +58,7 @@ int main(void)
 
 			//wait for x additional seconds
 			for (uint8_t i = 0; i < vR; i++){
-				_delay_ms(1000 - TICK_DURATION_MS);
+				_delay_ms(ONE_SECOND_MS - TICK_DURATION_MS);
 				
 			}
 			
@@ -66,7 +71,7 @@ int main(void)
 		}
 		else {
 			//normal case, just normal tick
-			_delay_ms(1000);
+			_delay_ms(ONE_SECOND_MS);
 			tick();
 		}
 	}
