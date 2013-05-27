785faac3811e8466c2d7bf11b8e9f5a8e26dfc08
alainvizzini@gmail.com <alainvizzini@gmail.com@a4e8eee6-8666-d384-cd2c-cc5a62ec2564>
2013-05-27 13:31:49 +0000

    Version 1.0, the original one. Forgot to put make a tag, now it's done.

---
+++ AlanOriginalVersion/setFuses.328.bat
@@ -0,0 +1,2 @@
+D:\arduino-1.0.2\hardware\tools\avr\bin\avrdude.exe -C D:\arduino-1.0.2\hardware\tools\avr\etc\avrdude.conf  -p m328p -c avrispv2 -P COM6 -U lfuse:w:0xf7:m -U hfuse:w:0xd9:m -U efuse:w:0xff:m
+pause
