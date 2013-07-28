067b3b60e541a91c42546c1326bf59055f0505c4
alainvizzini@gmail.com <alainvizzini@gmail.com@a4e8eee6-8666-d384-cd2c-cc5a62ec2564>
2013-07-28 15:33:24 +0000

    Added test. Head control mode via serial.

---
+++ trunk/LaserCutterController/Test_Head.h
@@ -0,0 +1,17 @@
+#include "Arduino.h"
+#include "HeadAxisControl.h"
+
+#ifndef __TestHead_h__
+#define __TestHead_h__
+
+void TestHead_FindBoundaries() {
+  //Go left...
+  moveHeadByAmount(-1000);
+  //reset the position
+  mHeadPos=0;
+  //...and go all the way right
+  moveHeadByAmount(+2000);
+  
+}
+
+#endif __TestHead_h__
