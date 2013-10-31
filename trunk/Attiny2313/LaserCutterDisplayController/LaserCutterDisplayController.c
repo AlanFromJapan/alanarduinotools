2e9d52b2f82a1c8ab866cbe6cfcee2d8f553ce3e
alainvizzini@gmail.com <alainvizzini@gmail.com@a4e8eee6-8666-d384-cd2c-cc5a62ec2564>
2013-10-31 14:57:44 +0000

    Loop for receiving and writing text ok (trick was that the master close the bus after each line and wait a few ms)

--- trunk/Attiny2313/LaserCutterDisplayController/LaserCutterDisplayController.c
+++ trunk/Attiny2313/LaserCutterDisplayController/LaserCutterDisplayController.c
@@ -14,8 +14,10 @@
 
 #define I2C_SLAVE_ADDR  0x26            // i2c slave address (38)
 
+#define LCD_COMMAND_CLS		99
+
 #include <avr/io.h>
-#include <avr/delay.h>
+#include <util/delay.h>
 #include <inttypes.h>
 #include <avr/interrupt.h>
 
@@ -97,15 +99,103 @@ void testTwiString(){
 		//_delay_ms(100);
 	}
 }
+
+
+static uint8_t mBuff[LCD_MAXCHAR_PER_LINE+1];
+
+void loopTwiString(){
+	LcdSetCursor(0,0);
 	
+	while(1)
+	{
+		uint8_t vReceived = 0;
+		uint8_t vPos = 0;
+		//message format is 1 byte (line), the rest is message
+		if (usiTwiDataInReceiveBuffer()){
+			vReceived = usiTwiReceiveByte();
+			
+			if (vReceived == LCD_COMMAND_CLS){
+				//clear screen
+				LcdClear();
+			}
+			else {
+				//other, write text
+				LcdSetCursor(0, vReceived);
+								
+				//force the last value to not be 0 for the do while
+				vReceived = 0xff;
+				vPos = 0;
+				
+				do {
+					if (usiTwiDataInReceiveBuffer()){
+						//data are ready
+						vReceived = usiTwiReceiveByte();
+						mBuff[vPos] = vReceived;
+						vPos++;
+					}
+				} while (vReceived != 0 && vPos < LCD_MAXCHAR_PER_LINE);
+			
+		
+				//just in case force a null terminated string
+				mBuff[LCD_MAXCHAR_PER_LINE] = 0;
+				LcdWrite(mBuff);
+			}			
+		}
+		
+	}
+}
+	
+	/*
+void testLines(){
+	LcdSetCursor(0,0);
+	
+	LcdSetCursor(0, 0);
+	strncpy(mBuff, "******zero", LCD_MAXCHAR_PER_LINE);
+	mBuff[LCD_MAXCHAR_PER_LINE] = 0;
+	LcdWrite(mBuff);
+
+
+	LcdSetCursor(0, 1);
+	strncpy(mBuff, " un", LCD_MAXCHAR_PER_LINE);
+	mBuff[LCD_MAXCHAR_PER_LINE] = 0;
+	LcdWrite(mBuff);
+
+
+	LcdSetCursor(0, 2);
+	strncpy(mBuff, "    deux", LCD_MAXCHAR_PER_LINE);
+	mBuff[LCD_MAXCHAR_PER_LINE] = 0;
+	LcdWrite(mBuff);
+
+
+	LcdSetCursor(0, 3);
+	strncpy(mBuff, "   trois", LCD_MAXCHAR_PER_LINE);
+	mBuff[LCD_MAXCHAR_PER_LINE] = 0;
+	LcdWrite(mBuff);
+
+//
+	//LcdSetCursor(0, 4);
+	//strncpy(mBuff, "quatre*", LCD_MAXCHAR_PER_LINE);
+	//mBuff[LCD_MAXCHAR_PER_LINE] = 0;
+	//LcdWrite(mBuff);
+
+
+	LcdSetCursor(0, 5);
+	strncpy(mBuff, "  cinq", LCD_MAXCHAR_PER_LINE);
+	mBuff[LCD_MAXCHAR_PER_LINE] = 0;
+	LcdWrite(mBuff);
+
+
+}
+		*/
 int main(void)
 {
+	setupTwiSlave();
+	
 	LcdSetup();
 	LcdSetPower(1);
 	
 	LcdClear();
 	
-	setupTwiSlave(); 
 	  
-	testTwiString();
+	loopTwiString();	
 }
