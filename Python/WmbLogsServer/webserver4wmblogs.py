ff72bc63d7ff0503d92bc5752caf4f9ca6dc6c32
alainvizzini@gmail.com <alainvizzini@gmail.com@a4e8eee6-8666-d384-cd2c-cc5a62ec2564>
2014-08-17 02:40:46 +0000

    Small modifications: moved all the code into the webserver class.

--- Python/WmbLogsServer/webserver4wmblogs.py
+++ Python/WmbLogsServer/webserver4wmblogs.py
@@ -24,78 +24,105 @@ reLine = re.compile(LINE_IN_FORMAT, re.IGNORECASE)
 dictErrors = dict()
 dictWellKnownBipCodeStyles = dict(BIP2153I="prioHigh", BIP2176S="prioLow")
 
-###########################################
-##  Process requests
-###########################################
 
-def handle_getContentAsXML(s):
-    '''s is the HttpRequestHandler 'self'.
-    This method returns the content of the file.'''
-    fin = file(name=r"D:\temp\wmbmessages.log.20140811byAVI\wmbmessages.log.20140811byAVI", mode="r")
 
-    dictErrors.clear()
+class WellKnownError:
+    def __init__(self, bipCode, messageContains,errorPriority,errorLabel):
+        self.bipCode=bipCode
+        self.messageContains =  messageContains
+        self.errorPriority = errorPriority
+        self.errorLabel = errorLabel
+        
+listWellKnownErrors = [
+WellKnownError(bipCode=None, messageContains=r"FAXECMServiceSFDCMainFlow#FCMComposite_1_8.CreateFormImageSFDCSubFlow#FCMComposite_1_3", errorPriority=1, errorLabel="Timeout on FormServer report generation. Ignore unless user complains.")
+    ]
+
+
+
     
-    s.do_HEAD()
+###########################################
+##  Web server 
+###########################################
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    'Source from https://wiki.python.org/moin/BaseHttpServer'
+    
+    ###########################################
+    ##  Process requests
+    ###########################################
+    def getWellknownErrorDetails (s, bipCode, rawContent):
+        for wke in listWellKnownErrors:
+            if (wke.bipCode != None and wke.bipCode == bipCode) or (wke.messageContains != None and rawContent != None and wke.messageContains in rawContent):
+                return wke
+        return None
+    
+    def handle_getContentAsXML(s):
+        '''s is the HttpRequestHandler 'self'.
+        This method returns the content of the file.'''
+        fin = file(name=r"D:\temp\wmbmessages.log.20140811byAVI\wmbmessages.log.20140811byAVI", mode="r")
 
-    s.wfile.write("""<?xml version="1.0" encoding="UTF-8"?>
+        dictErrors.clear()
+        
+        s.do_HEAD()
+
+        s.wfile.write("""<?xml version="1.0" encoding="UTF-8"?>
 <?xml-stylesheet type="text/xsl" href="Transform.xsl"?>
 """)
 
-    s.wfile.write("<logItems>")
-    i = 0
-    for line in fin:
-        #s.wfile.write(line)
-        match = reLine.match(line)
-        if match != None:
-            bipcode=match.group('bipcode')
-            if bipcode != None:
-                if bipcode in dictErrors:
-                    dictErrors[bipcode] = (bipcode, dictErrors[bipcode][1] + 1)
-                else:
-                    dictErrors[bipcode] = (bipcode, 1)
-                    
-            s.wfile.write(LINE_OUT_FORMATXML.format(
-                time=match.group('time'),
-                bipcode=bipcode,
-                msgTitle=match.group('msgTitle'),
-                bipcount=str(dictErrors[bipcode][1]),
-                prioStyle=("prioDefault" if not bipcode in dictWellKnownBipCodeStyles else dictWellKnownBipCodeStyles[bipcode]),
-                rawContent=(line if bipcode != "BIP3051E" else wmbErrorParser.parseFormatWmbError(match.group("theRest"))), 
-                ))
-        else:
-            s.wfile.write("Unknown format line:" + line)
+        s.wfile.write("<logItems>")
+        i = 0
+        for line in fin:
+            #s.wfile.write(line)
+            match = reLine.match(line)
+            if match != None:
+                bipcode=match.group('bipcode')
+                rawContent=match.group("theRest")
+                
+                if bipcode != None:
+                    if bipcode in dictErrors:
+                        dictErrors[bipcode] = (bipcode, dictErrors[bipcode][1] + 1)
+                    else:
+                        dictErrors[bipcode] = (bipcode, 1)
+                        
+                s.wfile.write(LINE_OUT_FORMATXML.format(
+                    time=match.group('time'),
+                    bipcode=bipcode,
+                    msgTitle=match.group('msgTitle'),
+                    bipcount=str(dictErrors[bipcode][1]),
+                    prioStyle=("prioDefault" if not bipcode in dictWellKnownBipCodeStyles else dictWellKnownBipCodeStyles[bipcode]),
+                    rawContent=(rawContent if bipcode != "BIP3051E" else wmbErrorParser.parseFormatWmbError(rawContent)), 
+                    ))
+            else:
+                s.wfile.write("Unknown format line:" + line)
 
-        #dummy limit to 100 for test
-        i += 1
-        if i > 100:
-            break
-    
-    s.wfile.write("</logItems>")
-    
-    fin.close()
-
-
-
-
-def handle_getFile (s, path):
-    path = """D:\\Subversion\\Python\\WmbLogsServer\\""" + path
-    f = None
-    try:
-        s.do_HEAD()
-        print ("serving : '" + path  +"'")
+            #dummy limit to 100 for test
+            i += 1
+            if i > 100:
+                break
         
-        f = open (path, mode="r")
-        shutil.copyfileobj(f, s.wfile)
-    finally:
-        if f != None:
-            f.close()
-    
-###########################################
-##  Web server part
-###########################################
+        s.wfile.write("</logItems>")
+        
+        fin.close()
 
-class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    'Source from https://wiki.python.org/moin/BaseHttpServer'
+
+
+
+    def handle_getFile (s, path):
+        path = """D:\\Subversion\\Python\\WmbLogsServer\\""" + path
+        f = None
+        try:
+            s.do_HEAD()
+            print ("serving : '" + path  +"'")
+            
+            f = open (path, mode="r")
+            shutil.copyfileobj(f, s.wfile)
+        finally:
+            if f != None:
+                f.close()
+
+    
+    ###########################################
+    ##  Base HTTP server (GET request management)
+    ###########################################            
     def do_HEAD(s):
         s.send_response(200)
         if len(s.path) > 4 and s.path[-3:] == "xml":
@@ -114,11 +141,11 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
         #First: triage of the request
         if s.path == "/XXX.xml":
-            handle_getContentAsXML (s)
+            s.handle_getContentAsXML ()
         elif s.path == "/Styles.css":
-            handle_getFile (s, s.path[1:])
+            s.handle_getFile (s.path[1:])
         elif s.path == "/Transform.xsl":
-            handle_getFile (s, s.path[1:])
+            s.handle_getFile (s.path[1:])
         else:
             s.send_response(404)
   
