1e211ef0f869398207cd363527559492bf84d53c
alainvizzini@gmail.com <alainvizzini@gmail.com@a4e8eee6-8666-d384-cd2c-cc5a62ec2564>
2014-11-23 10:23:16 +0000

    Added new project MultiClockManager to replace the Arduino code of the AlanNumitron clock and al. It compiles and draws a few things on a 5x5 wordclock shield.

---
+++ Atmegaxx8/MultiClockManager/DS3234.h
@@ -0,0 +1,163 @@
+
+#ifndef __DS3234_h__
+#define __DS3234_h__
+
+//Include also in the MAIN .ino file!
+#include "SPI.h"
+#include "MCMShared.h"
+
+#define RTC_DS3234
+
+#include <util/delay.h>
+
+//Chip select pin. You must use this, front detection is necessary
+//#define DS3234_PIN_CS 10  --> SPI_PIN_SS = PIN B2
+
+
+
+
+//=====================================
+uint8_t setupDS3234(uint8_t pSetRegisters){ 
+   //pinMode(DS3234_PIN_CS,OUTPUT); // chip select
+   DDRB |= (1 << SPI_PIN_SS);
+   
+   // start the SPI library:
+   spiBegin();
+   spiSetBitOrder(MSBFIRST); 
+   spiSetDataMode(SPI_MODE1); // both mode 1 & 3 should work 
+   if (pSetRegisters != 0){
+	   //set control register 
+	   PORTB &= ~(1 << SPI_PIN_SS); //digitalWrite(DS3234_PIN_CS, LOW);  
+	   spiTransfer(0x8E);
+	   spiTransfer(0x60); //60= disable Osciallator and Battery SQ wave @1hz, temp compensation, Alarms disabled
+	   PORTB |= (1 << SPI_PIN_SS); //digitalWrite(DS3234_PIN_CS, HIGH);
+   }
+   
+   _delay_ms(10); //delay(10);
+
+   return 0;
+}
+
+
+//=====================================
+//Taken from Sparfun sample (as is)
+uint8_t SetTimeDate(uint8_t d, uint8_t mo, uint8_t y, uint8_t h, uint8_t mi, uint8_t s){ 
+   uint8_t TimeDate [7]={
+      s,mi,h,0,d,mo,y               };
+   for(uint8_t i=0; i<=6;i++){
+      if(i==3)
+         i++;
+      uint8_t b= TimeDate[i]/10;
+      uint8_t a= TimeDate[i]-b*10;
+      if(i==2){
+         if (b==2)
+            b=0b00000010;
+         else if (b==1)
+            b=0b00000001;
+      }	
+      TimeDate[i]= a+(b<<4);
+
+      PORTB &= ~(1 << SPI_PIN_SS); //digitalWrite(DS3234_PIN_CS, LOW);
+      spiTransfer(i+0x80); 
+      spiTransfer(TimeDate[i]);        
+      PORTB |= (1 << SPI_PIN_SS); //digitalWrite(DS3234_PIN_CS, HIGH);
+   }
+
+   return 0;
+}
+
+
+
+//=====================================
+//Taken from Sparfun sample (amended to return values and not a string)
+//Parameter you pass must be a correctly initialized 7 or more uint8_t array
+void ReadTimeArray(uint8_t* TimeDate){
+   //uint8_t TimeDate [7]; //second,minute,hour,null,day,month,year		
+   for(uint8_t i=0; i<=6;i++){
+      if(i==3)
+         i++;
+      PORTB &= ~(1 << SPI_PIN_SS); //digitalWrite(DS3234_PIN_CS, LOW);
+      spiTransfer(i+0x00); 
+      uint8_t n = spiTransfer(0x00);        
+      PORTB |= (1 << SPI_PIN_SS); //digitalWrite(DS3234_PIN_CS, HIGH);
+      uint8_t a=n & 0b00001111;    
+      if(i==2){	
+         uint8_t b=(n & 0b00110000)>>4; //24 hour mode
+         if(b==0b00000010)
+            b=20;        
+         else if(b==0b00000001)
+            b=10;
+         *(TimeDate +i)=a+b;
+      }
+      else if(i==4){
+         uint8_t b=(n & 0b00110000)>>4;
+         *(TimeDate +i)=a+b*10;
+      }
+      else if(i==5){
+         uint8_t b=(n & 0b00010000)>>4;
+         *(TimeDate +i)=a+b*10;
+      }
+      else if(i==6){
+         uint8_t b=(n & 0b11110000)>>4;
+         *(TimeDate +i)=a+b*10;
+      }
+      else{	
+         uint8_t b=(n & 0b01110000)>>4;
+         *(TimeDate +i)=a+b*10;	
+      }
+   }
+}
+
+
+
+
+//=====================================
+//Taken from Sparfun sample (amended to return values and not a string)
+//Parameter you pass must be a correctly initialized 7 or more uint8_t array
+void ReadTime(Date* TimeDate){
+   //uint8_t TimeDate [7]; //second,minute,hour,null,day,month,year		
+   for(uint8_t i=0; i<=6;i++){
+      if(i==3)
+         i++;
+      PORTB &= ~(1 << SPI_PIN_SS); //digitalWrite(DS3234_PIN_CS, LOW);
+      spiTransfer(i+0x00); 
+      uint8_t n = spiTransfer(0x00);        
+      PORTB |= (1 << SPI_PIN_SS); //digitalWrite(DS3234_PIN_CS, HIGH);
+      uint8_t a=n & 0b00001111;    
+      if(i==2){	
+         uint8_t b=(n & 0b00110000)>>4; //24 hour mode
+         if(b==0b00000010)
+            b=20;        
+         else if(b==0b00000001)
+            b=10;
+         (*TimeDate).hour=a+b;
+      }
+      else if(i==4){
+         uint8_t b=(n & 0b00110000)>>4;
+         (*TimeDate).dayOfMonth=a+b*10;
+      }
+      else if(i==5){
+         uint8_t b=(n & 0b00010000)>>4;
+         (*TimeDate).month=a+b*10;
+      }
+      else if(i==6){
+         uint8_t b=(n & 0b11110000)>>4;
+         (*TimeDate).year=a+b*10;
+      }
+      else{ 
+         if (i == 0) {
+            uint8_t b=(n & 0b01110000)>>4;
+            (*TimeDate).second = a+b*10;	
+         }	
+         if (i == 1) {
+            uint8_t b=(n & 0b01110000)>>4;
+            (*TimeDate).minute = a+b*10;	
+         }	
+
+      }
+   }
+}
+
+#endif // __DS3234_h__
+
+
---
+++ Atmegaxx8/MultiClockManager/MultiClockManager.c
@@ -0,0 +1,130 @@
+/*
+ * MultiClockManager.c
+ *
+ * At last, I start the portage to AtmelStudio of the WordClock project made under Arduino.
+ * This program is made to serve all the clocks I produced, disregarding the RTC or display used.
+ * http://kalshagar.wikispaces.com/WordClock
+ *
+ * Created: 2014/11/23 11:23:47
+ *  Author: Alan
+ *
+ * Migration Status:
+ *   RTC:
+ *     x DS3231
+ *     x DS3234
+ *     x EPSON RTC 4543 SA/SB
+ *   Display:
+ *     x Numitron
+ *     x Voltmeter
+ * 
+ */ 
+
+//for delay.h
+#define F_CPU 16000000L
+
+//System includes
+#include <avr/io.h>
+
+//Program includes
+#include "MCMShared.h"
+
+//Choose what RTC to use
+//#include "DS3234.h"
+#include "RTCFake.h"
+
+//Choose the display to use
+#include "MCMLedMatrix.h"
+#include "WordclockLayouts.h"
+#define USE_DISPLAY_BCD1
+
+
+/************************************************************************/
+/* Read time functions                                                  */
+/************************************************************************/
+void mainReadTime(Date* pTime){
+	
+	#ifdef RTC_DS3231
+	getDateDS3231(pTime);
+	#endif //RTC_DS3231
+
+	#ifdef RTC_DS3234
+	ReadTime(pTime);
+	#endif //RTC_DS3234
+
+	#ifdef RTC_RTC4543
+	ReadTime4543(pTime);
+	#endif //RTC_RTC4543
+	
+	#ifdef RTC_FAKE
+	ReadTime(pTime);
+	#endif //RTC_FAKE
+}
+
+/************************************************************************/
+/* Setup                                                                */
+/************************************************************************/
+void setup() {
+
+#ifdef RTC_DS3234
+	setupDS3234(1);
+	
+	//README TODO REMOVE ME : just for the testing purpose now
+	SetTimeDate(25,05,2013,18,14,00);
+#endif //RTC_DS3234	
+
+//TODO: surround with #ifdef
+setupLedMatrix(); //SETUP_MATRIX();
+}
+
+
+
+/************************************************************************/
+/* Main                                                                 */
+/************************************************************************/
+int main(void) {
+	setup();
+	
+	Date vLastDate;
+
+	uint8_t i = 0;	
+	resetLedMatrix();
+	setupLedMatrix();
+	
+	
+	setCell(i,i,1);i++;
+	setCell(i,i,1);i++;
+	setCell(i,i,1);i++;
+	setCell(i,i,1);i++;
+	setCell(i,i,1);i++;
+	
+	//setCell(0, 2, 1);
+	//setCell(0, 4, 1);
+	//
+	setCell(3, 4, 1);
+
+	setCell(4, 3, 1);
+	setCell(4, 0, 1);
+	
+    while(1) {
+       mainReadTime(&vLastDate);
+		
+		//MapTimeInLedMatrix_BCD1(&vLastDate); //MAP_MATRIX_MFUNC(&vLastDate);
+		
+		drawLedMatrix(); //DRAW_MATRIX_FUNC();
+		
+		
+		//PORTB = 0x03; //=B1 digitalWrite(r + WCLK_ROWA, HIGH);
+		//PORTD = 0xe4; //digitalWrite(WCLK_COL1- c, LOW); //minus because reverse order
+//
+		//PORTD &= ~(1 << 7);
+		////POV
+		//_delay_us(WCLK_POV_DURATION); // delayMicroseconds(WCLK_POV_DURATION);
+//
+		////reverse the pins HIGH/LOW status to turn it off
+		//PORTB = 0xff; //=B1 digitalWrite(r + WCLK_ROWA, LOW);
+		//PORTD = 0xff; //digitalWrite(WCLK_COL1- c, HIGH); //minus because reverse order
+		
+		
+		_delay_ms(50);
+    }
+}
