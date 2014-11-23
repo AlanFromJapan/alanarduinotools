9c258caea7b2eef7c2532e62f40a054108106b71
alainvizzini@gmail.com <alainvizzini@gmail.com@a4e8eee6-8666-d384-cd2c-cc5a62ec2564>
2014-11-23 11:09:17 +0000

    Added transposition of data in the led matrix as a cheap and easy trick to fix drawing orientation.

--- trunk/Atmegaxx8/MultiClockManager/MCMLedMatrix.h
+++ trunk/Atmegaxx8/MultiClockManager/MCMLedMatrix.h
@@ -4,18 +4,25 @@
 
 #include <util/delay.h>
 
+//THE in memory matrix when using led matrix display
 #define WCLK_MATRIX_SIZE 5
 uint8_t WCLK_LED_MATRIX[WCLK_MATRIX_SIZE];
 
-#define WCLK_COL1LEFT (WCLK_MATRIX_SIZE * 2)
+//#define WCLK_COL1LEFT (WCLK_MATRIX_SIZE * 2)
 
 //duration in us of the illumination of the current led while drawing
 #define WCLK_POV_DURATION 200
 
 //ALL Reversed -> all leds off : PORTB.PORTD = 0bxxxxxx11.11100000
 #define LEDS_ALL_OFF() 	{ PORTB |= 0x03; PORTD = 0xe0; }
-	
 
+
+//transpose the matrix (row <=> columns): used for legacy layouts
+#define LED_MATRIX_TRANSPOSE	
+
+/************************************************************************/
+/* Reset matrix to empty values (blank)                                 */
+/************************************************************************/
 void resetLedMatrix(){
    //reset matrix
    for (uint8_t i=0; i < WCLK_MATRIX_SIZE; i++){
@@ -23,6 +30,9 @@ void resetLedMatrix(){
    }
 }
 
+/************************************************************************/
+/* Initialize the pins for using led matrix                             */
+/************************************************************************/
 void setupLedMatrix() {
    resetLedMatrix();
 
@@ -48,9 +58,22 @@ void setupLedMatrix2x1() {
 }
 
 
+//simple permutation of 2 parameters
+inline void _transposeColRow (uint8_t *col, uint8_t *row) { 
+	uint8_t xxxx = *col; 
+	*col = *row; 
+	*row = xxxx; 
+}
 
-
+/************************************************************************/
+/* Set a cell of the matrix to a given value                            */
+/************************************************************************/
 void setCell (uint8_t row, uint8_t col, uint8_t val){
+#ifdef LED_MATRIX_TRANSPOSE	
+	//cheap ugly trick, transposes row and column (too lazy to rewrite the drawLedMatrix)
+	_transposeColRow(&col, &row);
+#endif //LED_MATRIX_TRANSPOSE
+
    if (val){
       WCLK_LED_MATRIX[row] |= (1 << col);
    }
@@ -59,42 +82,34 @@ void setCell (uint8_t row, uint8_t col, uint8_t val){
    }
 }
 
+/************************************************************************/
+/* Toggle a cell of the matrix to a given value                            */
+/************************************************************************/
 void toggleCell (uint8_t row, uint8_t col){
+#ifdef LED_MATRIX_TRANSPOSE
+	//cheap ugly trick, transposes row and column (too lazy to rewrite the drawLedMatrix)
+	_transposeColRow(&col, &row);
+#endif //LED_MATRIX_TRANSPOSE
+	
    WCLK_LED_MATRIX[row] = WCLK_LED_MATRIX[row] ^ (1 << col);
 }
 
-//Draws the in memory matrix to the led board.
-//You will most likely rewrite this function unless you use direct wiring like is planned on the main board.
-//Use this as a source of inspiration and make your own in case of custom wiring.
-//If you want to gain some time, replace the digitalWrite by some pseudoC code.
+/************************************************************************/
+/* Draws the in memory matrix to the led board.                         */
+/* You will most likely rewrite this function unless you use direct wiring like is planned on the main board.*/
+/************************************************************************/
 void drawLedMatrix (){
 	//ALL Reversed -> all leds off : PORTB.PORTD = 0bxxxxxx11.11100000 
 	LEDS_ALL_OFF();
 	
    //this does NOT run in constant time because we only turns led on, those off are already off so they are skipped.
    //side effect: the more there is led on, the slower it is to draw it.
-/*   for (uint8_t r=0; r < WCLK_MATRIX_SIZE; r++){
-      for (uint8_t c=0; c < WCLK_MATRIX_SIZE; c++){
-         if (0 != (WCLK_LED_MATRIX[r] & (1 << c))){
-            //turn the led on
-            digitalWrite(r + WCLK_ROWA, HIGH);
-            digitalWrite(WCLK_COL1- c, LOW); //minus because reverse order
-
-            //POV
-            delayMicroseconds(WCLK_POV_DURATION);
-
-            //reverse the pins HIGH/LOW status to turn it off
-            digitalWrite(r + WCLK_ROWA, LOW);
-            digitalWrite(WCLK_COL1- c, HIGH); //minus because reverse order
-         }
-      }   
-   }*/
    
    uint8_t r = 0;
    uint8_t rMask = 0;
    //Raw 1 (top)
    r=0;
-   rMask = 0x01;
+   rMask = 0x01; //inverse logic (makes B1 = 0 => sink current)
    for (uint8_t c=0; c < WCLK_MATRIX_SIZE; c++){
 	   if (0 != (WCLK_LED_MATRIX[r] & (1 << c))){
 		   //turn the led on
@@ -111,7 +126,7 @@ void drawLedMatrix (){
    
 
    r=1;
-   rMask = 0x02;
+   rMask = 0x02; //inverse logic (makes B0 = 0 => sink current)
    for (uint8_t c=0; c < WCLK_MATRIX_SIZE; c++){
 	   if (0 != (WCLK_LED_MATRIX[r] & (1 << c))){
 		   //turn the led on
@@ -180,52 +195,5 @@ void drawLedMatrix (){
 
 }
 
-/*
-void drawLedMatrix2x1 (){
-   //COLUMNS are NEGATIVE side, so turn them off by putting them all HIGH
-   for (uint8_t i=WCLK_COL1; i> WCLK_COL1 - WCLK_MATRIX_SIZE; i--){
-      digitalWrite(i, HIGH);
-   } 
-   for (uint8_t i=WCLK_COL1LEFT; i< WCLK_COL1LEFT + WCLK_MATRIX_SIZE; i++){
-      digitalWrite(i, HIGH);
-   } 
-
-   //this does NOT run in constant time because we only turns led on, those off are already off so they are skipped.
-   //side effect: the more there is led on, the slower it is to draw it.
-   for (uint8_t r=0; r < WCLK_MATRIX_SIZE; r++){
-      //Left panel
-      for (uint8_t c=0; c < WCLK_MATRIX_SIZE; c++){
-         if (0 != (WCLK_LED_MATRIX[r] & (1 << c))){
-            //turn the led on
-            digitalWrite(r + WCLK_ROWA, HIGH);
-            digitalWrite(WCLK_COL1LEFT +(WCLK_MATRIX_SIZE -1) -c, LOW);
-
-            delayMicroseconds(WCLK_POV_DURATION);
-
-            //reverse the pins HIGH/LOW status to turn it off
-            digitalWrite(r + WCLK_ROWA, LOW);
-            digitalWrite(WCLK_COL1LEFT +(WCLK_MATRIX_SIZE -1) -c, HIGH);
-         }
-      }  
-
-      //Right panel
-      for (uint8_t c=WCLK_MATRIX_SIZE; c < (2 * WCLK_MATRIX_SIZE); c++){
-         if (0 != (WCLK_LED_MATRIX[r] & (1 << c))){
-            //turn the led on
-            digitalWrite(r + WCLK_ROWA, HIGH);
-            digitalWrite(WCLK_COL1 -(c-WCLK_MATRIX_SIZE), LOW);
-
-            delayMicroseconds(WCLK_POV_DURATION);
-
-            //reverse the pins HIGH/LOW status to turn it off
-            digitalWrite(r + WCLK_ROWA, LOW);
-            digitalWrite(WCLK_COL1-(c-WCLK_MATRIX_SIZE) , HIGH);
-         }
-      }   
-
-
-   }
-}
-*/
 
 #endif //__MCMLedMatrix_h__
