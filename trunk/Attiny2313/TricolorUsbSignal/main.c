0a010d0a51e0d57e357506166024903a17366bc3
alainvizzini@gmail.com <alainvizzini@gmail.com@a4e8eee6-8666-d384-cd2c-cc5a62ec2564>
2014-12-20 08:08:33 +0000

    Moved USB code to vusb-config.h

--- trunk/Attiny2313/TricolorUsbSignal/main.c
+++ trunk/Attiny2313/TricolorUsbSignal/main.c
@@ -16,6 +16,7 @@
 
 #include "usbdrv.h"
 #include "Keyboard.h"
+#include "vusb-config.h"
 
 /* ----------------------- hardware I/O abstraction ------------------------ */
 
@@ -25,14 +26,12 @@ PB1	Key 2
 PB2	Key 3
 PB3	Key 4
 PB4	Key 5
-PB5 Key 6
+PB5 red LED
+PB6 yellow LED
+PB7	green LED
 
-PC0	Key 7
-PC1	Key 8
-PC2	Key 9
-PC3	Key 10
-PC4	Key 11
-PC5	Key 12
+PA0 
+PA1
 
 PD0	USB-
 PD1	debug tx
@@ -41,15 +40,14 @@ PD3	Key 13
 PD4	Key 14
 PD5	Key 15
 PD6	Key 16
-PD7	Key 17
 */
 
 static void hardwareInit(void)
 {
 uchar	i, j;
 
-    PORTB = 0xff;   /* activate all pull-ups */
-    DDRB = 0;       /* all pins input */
+    PORTB = 0x1f;   /* activate all pull-ups EXCEPT on leds */
+    DDRB = 0xe0;       /* all pins input EXCEPT on leds that are outputs */
     
     PORTD = 0xfa;   /* 1111 1010 bin: activate pull-ups except on USB lines */
     DDRD = 0x07;    /* 0000 0111 bin: all pins input except USB (-> USB reset) */
@@ -67,61 +65,7 @@ uchar	i, j;
 
 #define NUM_KEYS    17
 
-/* The following function returns an index for the first key pressed. It
- * returns 0 if no key is pressed.
- */
-static uchar    keyPressed(void)
-{
-uchar   i, mask, x;
 
-    x = PINB;
-    mask = 1;
-    for(i=0;i<6;i++){
-        if((x & mask) == 0)
-            return i + 1;
-        mask <<= 1;
-    }
-    
-	
-    return 0;
-}
-
-/* ------------------------------------------------------------------------- */
-/* ----------------------------- USB interface ----------------------------- */
-/* ------------------------------------------------------------------------- */
-
-static uchar    reportBuffer[2];    /* buffer for HID reports */
-static uchar    idleRate;           /* in 4 ms units */
-
-const PROGMEM char usbHidReportDescriptor[35] = {   /* USB report descriptor */
-    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
-    0x09, 0x06,                    // USAGE (Keyboard)
-    0xa1, 0x01,                    // COLLECTION (Application)
-    0x05, 0x07,                    //   USAGE_PAGE (Keyboard)
-    0x19, 0xe0,                    //   USAGE_MINIMUM (Keyboard LeftControl)
-    0x29, 0xe7,                    //   USAGE_MAXIMUM (Keyboard Right GUI)
-    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-    0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
-    0x75, 0x01,                    //   REPORT_SIZE (1)
-    0x95, 0x08,                    //   REPORT_COUNT (8)
-    0x81, 0x02,                    //   INPUT (Data,Var,Abs)
-    0x95, 0x01,                    //   REPORT_COUNT (1)
-    0x75, 0x08,                    //   REPORT_SIZE (8)
-    0x25, 0x65,                    //   LOGICAL_MAXIMUM (101)
-    0x19, 0x00,                    //   USAGE_MINIMUM (Reserved (no event indicated))
-    0x29, 0x65,                    //   USAGE_MAXIMUM (Keyboard Application)
-    0x81, 0x00,                    //   INPUT (Data,Ary,Abs)
-    0xc0                           // END_COLLECTION
-};
-/* We use a simplifed keyboard report descriptor which does not support the
- * boot protocol. We don't allow setting status LEDs and we only allow one
- * simultaneous key press (except modifiers). We can therefore use short
- * 2 byte input reports.
- * The report descriptor has been created with usb.org's "HID Descriptor Tool"
- * which can be downloaded from http://www.usb.org/developers/hidpage/.
- * Redundant entries (such as LOGICAL_MINIMUM and USAGE_PAGE) have been omitted
- * for the second INPUT item.
- */
 
 
 const PROGMEM uint16_t mMappingCharKeys[] = {
@@ -135,7 +79,7 @@ const PROGMEM uint16_t mMappingCharKeys[] = {
 char mText[] = "aadbc";
 uint8_t mTextPos = 0;
 
-static void buildReport(uchar key)
+void buildReport(uchar key)
 {
 /* This (not so elegant) cast saves us 10 bytes of program memory */
 //    *(int *)reportBuffer = pgm_read_word(keyReport[key]);
@@ -174,27 +118,6 @@ static void buildReport(uchar key)
 	reportBuffer[0] = 0;
 	reportBuffer[1] = 0;
 
-}
-uchar	usbFunctionSetup(uchar data[8])
-{
-usbRequest_t    *rq = (void *)data;
-
-    usbMsgPtr = reportBuffer;
-    if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS){    /* class request type */
-        if(rq->bRequest == USBRQ_HID_GET_REPORT){  /* wValue: ReportType (highbyte), ReportID (lowbyte) */
-            /* we only have one report type, so don't look at wValue */
-            buildReport(keyPressed());
-            return sizeof(reportBuffer);
-        }else if(rq->bRequest == USBRQ_HID_GET_IDLE){
-            usbMsgPtr = &idleRate;
-            return 1;
-        }else if(rq->bRequest == USBRQ_HID_SET_IDLE){
-            idleRate = rq->wValue.bytes[1];
-        }
-    }else{
-        /* no vendor specific requests implemented */
-    }
-	return 0;
 }
 
 /* ------------------------------------------------------------------------- */
---
+++ trunk/Attiny2313/TricolorUsbSignal/vusb-config.h
@@ -0,0 +1,87 @@
+/*
+ * vusb_config.h
+ *
+ * Created: 2014/12/20 17:01:26
+ *  Author: Alan
+ */ 
+
+
+#ifndef VUSBCONFIG_H_
+#define VUSBCONFIG_H_
+
+
+#include "usbdrv.h"
+#include "Keyboard.h"
+
+/* ------------------------------------------------------------------------- */
+/* ----------------------------- USB interface ----------------------------- */
+/* ------------------------------------------------------------------------- */
+
+static uchar    reportBuffer[2];    /* buffer for HID reports */
+static uchar    idleRate;           /* in 4 ms units */
+
+const PROGMEM char usbHidReportDescriptor[35] = {   /* USB report descriptor */
+    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
+    0x09, 0x06,                    // USAGE (Keyboard)
+    0xa1, 0x01,                    // COLLECTION (Application)
+    0x05, 0x07,                    //   USAGE_PAGE (Keyboard)
+    0x19, 0xe0,                    //   USAGE_MINIMUM (Keyboard LeftControl)
+    0x29, 0xe7,                    //   USAGE_MAXIMUM (Keyboard Right GUI)
+    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+    0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
+    0x75, 0x01,                    //   REPORT_SIZE (1)
+    0x95, 0x08,                    //   REPORT_COUNT (8)
+    0x81, 0x02,                    //   INPUT (Data,Var,Abs)
+    0x95, 0x01,                    //   REPORT_COUNT (1)
+    0x75, 0x08,                    //   REPORT_SIZE (8)
+    0x25, 0x65,                    //   LOGICAL_MAXIMUM (101)
+    0x19, 0x00,                    //   USAGE_MINIMUM (Reserved (no event indicated))
+    0x29, 0x65,                    //   USAGE_MAXIMUM (Keyboard Application)
+    0x81, 0x00,                    //   INPUT (Data,Ary,Abs)
+    0xc0                           // END_COLLECTION
+};
+/* We use a simplifed keyboard report descriptor which does not support the
+ * boot protocol. We don't allow setting status LEDs and we only allow one
+ * simultaneous key press (except modifiers). We can therefore use short
+ * 2 byte input reports.
+ * The report descriptor has been created with usb.org's "HID Descriptor Tool"
+ * which can be downloaded from http://www.usb.org/developers/hidpage/.
+ * Redundant entries (such as LOGICAL_MINIMUM and USAGE_PAGE) have been omitted
+ * for the second INPUT item.
+ */
+
+
+/* The following function returns an index for the first key pressed. It
+ * returns 0 if no key is pressed.
+ */
+uint8_t    keyPressed(void)
+{
+	//input is pulled up, just one button so really don't care where it's pushed.
+	//take the PINB input register, mask to keep the input pins and invert: should be 0x00 if unpressed
+	return ~(0xe0 | (PINB & 0x1f));
+}
+
+
+uint8_t	usbFunctionSetup(uint8_t data[8])
+{
+	usbRequest_t    *rq = (void *)data;
+
+	usbMsgPtr = reportBuffer;
+	if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS){    /* class request type */
+	if(rq->bRequest == USBRQ_HID_GET_REPORT){  /* wValue: ReportType (highbyte), ReportID (lowbyte) */
+	/* we only have one report type, so don't look at wValue */
+	buildReport(keyPressed());
+	return sizeof(reportBuffer);
+}else if(rq->bRequest == USBRQ_HID_GET_IDLE){
+	usbMsgPtr = &idleRate;
+	return 1;
+}else if(rq->bRequest == USBRQ_HID_SET_IDLE){
+	idleRate = rq->wValue.bytes[1];
+}
+    }else{
+        /* no vendor specific requests implemented */
+    }
+	return 0;
+}
+
+#endif /* VUSBCONFIG_H_ */
