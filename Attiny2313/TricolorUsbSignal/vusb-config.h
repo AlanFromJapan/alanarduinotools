4c5537ded272911088341ada9d32e45e810cc112
alainvizzini@gmail.com <alainvizzini@gmail.com@a4e8eee6-8666-d384-cd2c-cc5a62ec2564>
2014-12-23 14:16:14 +0000

    Removed unused code and comments. Will tag as vanilla flavor of "receive characters as HID"

--- Attiny2313/TricolorUsbSignal/vusb-config.h
+++ Attiny2313/TricolorUsbSignal/vusb-config.h
@@ -19,48 +19,7 @@
 
 static uchar    reportBuffer[2];    /* buffer for HID reports */
 static uchar    idleRate;           /* in 4 ms units */
-/*
-const PROGMEM char usbHidReportDescriptor[35 + 6 + 18] = {   // USB report descriptor 
-    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
-    0x09, 0x06,                    // USAGE (Keyboard)
-    0xa1, 0x01,                    // COLLECTION (Application)
-    0x05, 0x07,                    //   USAGE_PAGE (Keyboard)
-    0x19, 0xe0,                    //   USAGE_MINIMUM (Keyboard LeftControl)
-    0x29, 0xe7,                    //   USAGE_MAXIMUM (Keyboard Right GUI)
-    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-    0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
-    0x75, 0x01,                    //   REPORT_SIZE (1)
-    0x95, 0x08,                    //   REPORT_COUNT (8)
-    0x81, 0x02,                    //   INPUT (Data,Var,Abs)
-	
-	
-	0x95, 0x01,                    //   REPORT_COUNT (1)
-	0x75, 0x08,                    //   REPORT_SIZE (8)
-	0x81, 0x03,                    //   INPUT (Cnst,Var,Abs)
-		
-    0x95, 0x05,                    //   REPORT_COUNT (5)
-    0x75, 0x01,                    //   REPORT_SIZE (1)
-    0x05, 0x08,                    //   USAGE_PAGE (LEDs)
-    0x19, 0x01,                    //   USAGE_MINIMUM (Num Lock)
-    0x29, 0x05,                    //   USAGE_MAXIMUM (Kana)
-    0x91, 0x02,                    //   OUTPUT (Data,Var,Abs)
-    0x95, 0x01,                    //   REPORT_COUNT (1)
-    0x75, 0x03,                    //   REPORT_SIZE (3)
-    0x91, 0x03,                    //   OUTPUT (Cnst,Var,Abs)	
-	
-	//TRAILER
-    0x95, 0x01,                    //   REPORT_COUNT (1)
-    0x75, 0x08,                    //   REPORT_SIZE (8)
-//    0x15, 0x00,                    //   LOGICAL_MINIMUM (0) //if you add this it doesn't work
-    0x25, 0x65,                    //   LOGICAL_MAXIMUM (101)
-    0x19, 0x00,                    //   USAGE_MINIMUM (Reserved (no event indicated))
-    0x29, 0x65,                    //   USAGE_MAXIMUM (Keyboard Application)
-    0x81, 0x00,                    //   INPUT (Data,Ary,Abs)
-	
-	
-	
-    0xc0                           // END_COLLECTION
-};*/
+
 
 const PROGMEM char usbHidReportDescriptor[63] = {
 	0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
@@ -127,7 +86,7 @@ uint8_t	usbFunctionSetup(uint8_t data[8]){
 	if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS){    /* class request type */
 		if(rq->bRequest == USBRQ_HID_GET_REPORT){  /* wValue: ReportType (highbyte), ReportID (lowbyte) */
 			/* we only have one report type, so don't look at wValue */
-			buildReport2(0);
+			buildReportEmpty();
 			return sizeof(reportBuffer);
 		}
 		else 
@@ -147,7 +106,7 @@ uint8_t	usbFunctionSetup(uint8_t data[8]){
 			}
 		}
 		
-		
+		//[Receive USB: you need this!]
 		else if(rq->bRequest == USBRQ_HID_GET_PROTOCOL) {
 		    if (rq->wValue.bytes[1] < 1) {
 			    protocolVer = rq->wValue.bytes[1];
@@ -169,8 +128,20 @@ uint8_t	usbFunctionSetup(uint8_t data[8]){
 //This method is in charge of handling the SET REPORT coming from the host
 uint8_t usbFunctionWrite(uint8_t *data, uint8_t len) {
 	
-	//just toggle the leds
-	PORTB ^= 0xe0;
+	//Toggle leds according status
+	uint8_t vLedStatus = 0x00;
+	if ((data[0] & LED_CAPS) != 0x00){
+		vLedStatus |= 0x80;
+	}
+	if ((data[0] & LED_SCROLL) != 0x00){
+		vLedStatus |= 0x40;
+	}
+	if ((data[0] & LED_NUM) != 0x00){
+		vLedStatus |= 0x20;
+	}
+	
+	//put the 3 high pins (the 3 leds) to 0 and "or" it with the ledmask to turn them on
+	PORTB = (PORTB & 0x1f) | vLedStatus ;
 	
 	return 0x01;	
 }
