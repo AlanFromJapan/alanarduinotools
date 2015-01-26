e22ff663795e04043e6fd5b0b956f986475065e4
alainvizzini@gmail.com <alainvizzini@gmail.com@a4e8eee6-8666-d384-cd2c-cc5a62ec2564>
2015-01-26 14:38:58 +0000

    First beta of the GbReaper

---
+++ trunk/C_Sharp/GbReaper/GbReaper/Controls/UcRomViewer.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GbReaper.Controls {
+    public partial class UcRomViewer : UserControl {
+
+        protected Image mImage = null;
+        protected Point mMouseHover = Point.Empty;
+
+        public Image Image {
+            set { 
+                this.mImage = value;
+                this.UpdateSidebars();
+                this.Invalidate();
+            }
+        }
+
+
+        public UcRomViewer() {
+            InitializeComponent();
+        }
+
+        private void vbar_Scroll(object sender, ScrollEventArgs e) {
+            this.Invalidate();
+        }
+        private void hbar_Scroll(object sender, ScrollEventArgs e) {
+            this.Invalidate();
+        }
+
+        private void UcRomViewer_Load(object sender, EventArgs e) {
+
+        }
+
+        protected override void OnPaint(PaintEventArgs e) {
+            base.OnPaint(e);
+
+            if (mImage == null)
+                return;
+
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            
+            e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+            e.Graphics.DrawImage(this.mImage, -hbar.Value, -vbar.Value);//, e.ClipRectangle.Width - vbar.Width, e.ClipRectangle.Height - hbar.Height);
+
+            if (!this.mMouseHover.IsEmpty) {
+                e.Graphics.DrawRectangle(Pens.Red, this.mMouseHover.X, this.mMouseHover.Y, 8, 8);
+            }
+        }
+
+        private void UpdateSidebars() {
+            if (mImage == null)
+                return;
+
+            vbar.Minimum = 0;
+            vbar.Maximum = this.mImage.Height - this.Height + hbar.Height;
+            if (vbar.Value > vbar.Maximum)
+                vbar.Value = vbar.Maximum;
+
+            hbar.Minimum = 0;
+            hbar.Maximum = this.mImage.Width - this.Width + vbar.Width;
+            if (hbar.Value > hbar.Maximum)
+                hbar.Value = hbar.Maximum;
+
+        }
+
+        protected override void OnResize(EventArgs e) {
+            base.OnResize(e);
+
+            UpdateSidebars();
+
+            this.Invalidate();
+        }
+        protected override void OnMouseMove(MouseEventArgs e) {
+            base.OnMouseMove(e);
+            this.mMouseHover = this.PointToClient(Cursor.Position);
+
+
+            this.Invalidate();
+        }
+
+    }
+}
---
+++ trunk/C_Sharp/GbReaper/GbReaper/Forms/FrmMain.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace GbReaper {
+    partial class FrmMain {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ofdRom = new System.Windows.Forms.OpenFileDialog();
+            this.ucRomViewer1 = new GbReaper.Controls.UcRomViewer();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(736, 26);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(40, 22);
+            this.fileToolStripMenuItem.Text = "&File";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F4;
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.loadToolStripMenuItem.Text = "&Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(145, 6);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.quitToolStripMenuItem.Text = "&Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // ofdRom
+            // 
+            this.ofdRom.Filter = "Game boy roms (*.gb)|*.gb|All files|*.*";
+            this.ofdRom.Title = "Load a ROM";
+            // 
+            // ucRomViewer1
+            // 
+            this.ucRomViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ucRomViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ucRomViewer1.Location = new System.Drawing.Point(0, 26);
+            this.ucRomViewer1.Name = "ucRomViewer1";
+            this.ucRomViewer1.Size = new System.Drawing.Size(736, 622);
+            this.ucRomViewer1.TabIndex = 3;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 648);
+            this.Controls.Add(this.ucRomViewer1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmMain";
+            this.Text = "GB Reaper";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog ofdRom;
+        private Controls.UcRomViewer ucRomViewer1;
+    }
+}
+
