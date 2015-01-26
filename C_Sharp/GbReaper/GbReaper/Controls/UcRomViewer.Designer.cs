9c20dab642d9a6e0631dfca7348444ec439c72eb
alainvizzini@gmail.com <alainvizzini@gmail.com@a4e8eee6-8666-d384-cd2c-cc5a62ec2564>
2015-01-26 14:38:58 +0000

    First beta of the GbReaper

---
+++ C_Sharp/GbReaper/GbReaper/Controls/UcRomViewer.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace GbReaper.Controls {
+    partial class UcRomViewer {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.vbar = new System.Windows.Forms.VScrollBar();
+            this.hbar = new System.Windows.Forms.HScrollBar();
+            this.SuspendLayout();
+            // 
+            // vbar
+            // 
+            this.vbar.Dock = System.Windows.Forms.DockStyle.Right;
+            this.vbar.Location = new System.Drawing.Point(308, 0);
+            this.vbar.Name = "vbar";
+            this.vbar.Size = new System.Drawing.Size(17, 296);
+            this.vbar.TabIndex = 0;
+            this.vbar.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vbar_Scroll);
+            // 
+            // hbar
+            // 
+            this.hbar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.hbar.Location = new System.Drawing.Point(0, 279);
+            this.hbar.Name = "hbar";
+            this.hbar.Size = new System.Drawing.Size(308, 17);
+            this.hbar.TabIndex = 1;
+            this.hbar.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hbar_Scroll);
+            // 
+            // UcRomViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.hbar);
+            this.Controls.Add(this.vbar);
+            this.DoubleBuffered = true;
+            this.Name = "UcRomViewer";
+            this.Size = new System.Drawing.Size(325, 296);
+            this.Load += new System.EventHandler(this.UcRomViewer_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.VScrollBar vbar;
+        private System.Windows.Forms.HScrollBar hbar;
+    }
+}
---
+++ C_Sharp/GbReaper/GbReaper/Forms/FrmMain.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace GbReaper {
+    public partial class FrmMain : Form {
+        public FrmMain() {
+            InitializeComponent();
+        }
+
+        const int READ_BUFFER_LENGTH = 8*2;
+
+
+        private void FrmMain_Load(object sender, EventArgs e) {
+            int zoomFactor = 4;
+            Image vM = GetRomAsImage(@"D:\Gameboy.dev\ROMS\tetris_(v1.1)\Tetris.gb", zoomFactor);
+            ucRomViewer1.Image = vM;
+        }
+
+        private Image GetRomAsImage(string pFilename, int pZoomFactor) {
+            int vFileLength = (int)(new FileInfo(pFilename).Length);
+            int vTileH = 40;
+
+            using (BinaryReader vReader = new BinaryReader(new FileStream(pFilename, FileMode.Open, FileAccess.Read))) {
+                byte[] vBuff = new byte[READ_BUFFER_LENGTH];
+                int vReadBytes = 0;
+
+                Bitmap vBitOutput = new Bitmap(8 * pZoomFactor * vTileH, 8 * pZoomFactor * (1 + ((vFileLength /(2 *8)) * 8 * pZoomFactor) / (8 * pZoomFactor * vTileH)));
+                Graphics vGraphx = Graphics.FromImage(vBitOutput);
+                vGraphx.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+
+                Bitmap vBitBuff = new Bitmap(8, 8);
+
+                int vCounter = 0;
+                while ((vReadBytes = vReader.Read(vBuff, 0, READ_BUFFER_LENGTH)) > 0) {
+                    for (int line = 0; line < 8; line++) {
+                        for (int pix = 0; pix < 8; pix++) {
+                            int vVal = 0;
+                            vVal = vVal | (0x01 & (vBuff[2 * line] >> (8 - 1 - pix)));
+                            vVal = vVal | (0x02 & (vBuff[2 * line + 1] >> (8 - 1 - pix)));
+
+                            Color vColor = GetColorFromPalette(vVal);
+                            vBitBuff.SetPixel(pix, line, vColor);
+                        }
+                    }
+
+                    if (((vCounter * 8 * pZoomFactor) / vBitOutput.Width) * 8 * pZoomFactor >= vBitOutput.Height) {
+                        break;
+                    }
+
+                    int x = (vCounter * 8 * pZoomFactor) % vBitOutput.Width;
+                    int y = 8 * pZoomFactor * ((vCounter * 8 * pZoomFactor) / vBitOutput.Width);
+
+                    vGraphx.DrawImage(vBitBuff, x, y, 8 * pZoomFactor, 8 * pZoomFactor);
+
+
+                    vCounter++;
+                }
+
+                vGraphx.Dispose();
+
+                return vBitOutput;
+            }
+        }
+
+        private Color GetColorFromPalette(int pVal) {
+            switch (pVal) {
+                case 1:
+                    return Color.LightGray;
+                case 2:
+                    return Color.Gray;
+                case 3:
+                    return Color.DarkGray;
+                default:
+                    break;
+            }
+
+            return Color.White;
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e) {
+            if (DialogResult.OK == ofdRom.ShowDialog(this)) {
+                Image vM = GetRomAsImage(ofdRom.FileName, 4);
+                ucRomViewer1.Image = vM;
+            }
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
