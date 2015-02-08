05d8677631475a32d53251834dbf0cc91455e9b1
alainvizzini@gmail.com <alainvizzini@gmail.com@a4e8eee6-8666-d384-cd2c-cc5a62ec2564>
2015-02-08 06:14:43 +0000

    Dev continued, implemented basic text interface. Write and read implemented but not working yet.

--- AtmegaxxA/GbCartridgeKSController/GbCartridgeKSController.cpp
+++ AtmegaxxA/GbCartridgeKSController/GbCartridgeKSController.cpp
@@ -6,23 +6,38 @@
  */ 
 
 
+#define F_CPU 16000000	// 16 MHz oscillator.
+
 #include <avr/io.h>
+#include <avr/delay.h>
 //#include <avr/interrupt.h>
 #include <stddef.h>
 #include <stdlib.h>
 
-#define F_CPU 16000000	// 16 MHz oscillator.
 #define BaudRate 9600
 #define MYUBRR (F_CPU / 16 / BaudRate ) - 1
 
 #define TALKATIVE 
 
-#define DATABUS_WRITE_MODE { DDRA = 0xFF; }
-#define DATABUS_READ_MODE { DDRA = 0x00; }
+#define DATABUS_WRITE_MODE { DDRA = 0xFF; PORTA = 0x00; PINA = 0x00; }
+#define DATABUS_READ_MODE { DDRA = 0x00; PORTA = 0x00; PINA = 0x00; }
 
 #define ASCII0  48
 #define ASCII9  57
 #define ASCIIA  65
+
+#define PORT_ADDR_H			PORTE
+#define PORT_ADDR_M			PORTC
+#define PORT_ADDR_L			PORTF
+#define PORT_DATA			PORTA
+#define PORT_CONTROL		PORTB
+#define PIN_CE				0x08
+#define PIN_OE				0x10
+#define PIN_WE				0x20
+#define PIN_RST				0x40
+//mask to enable result of only our control pins
+#define PIN_CONTROLS_MASK	0x78
+
 
 void delayLong()
 {
@@ -132,64 +147,285 @@ uint8_t hexToInt(uint8_t pChar){
 	}
 }
 
-uint8_t flashGetByteDecode() {
-	uint8_t inByte = 0;         // incoming serial byte
 
+
+uint16_t flashSetAddressFromInput() {
+	uint8_t inByte = 0;         // incoming serial byte
+	uint16_t vAddress = 0x0000;
 	
 	//read the 3 address byte MSB first
 	inByte = serialRead() - ASCII0;
-	PORTE = (inByte << 6) | (PORTE & 0x3F);
+	//PORT_ADDR_H = (inByte << 6) | (PORTE & 0x3F);
+	//just for not IGNORE PORTE
+	PORT_ADDR_H = PORT_ADDR_H & 0x3f;
 
 	inByte = hexToInt(serialRead());
 	inByte = inByte << 4;
 	inByte = inByte | hexToInt(serialRead());
-	PORTC = inByte;
+	PORT_ADDR_M = inByte;
+	
+	vAddress = inByte;
+	vAddress = vAddress << 8;
 
 	inByte = hexToInt(serialRead());
 	inByte = inByte << 4;
 	inByte = inByte + hexToInt(serialRead());
-	PORTF = inByte;
+	PORT_ADDR_L = inByte;
 
+	vAddress = vAddress | (uint16_t)inByte;
 
-#ifdef TALKATIVE
-
-serialWriteString("GET addr: ");
-
-char vBuffChar[4];
-serialWriteString(itoa(PORTC, vBuffChar, 16));
-
-serialWriteString(" ");
-serialWriteString(itoa(PORTF, vBuffChar, 16));
-
-serialWriteString("\r\n");
-#endif //TALKATIVE	
-		
+	return vAddress;
 }
 
+uint8_t flashGetDataFromInput() {
+	uint8_t inByte = 0;         // incoming serial byte
+	
+	inByte = hexToInt(serialRead());
+	inByte = inByte << 4;
+	inByte = inByte | hexToInt(serialRead());
+	//don't set it now, we have to respect a proper sequence for writing
+	//PORTA = inByte;
+	
+	return inByte;
+}
+
+/************************************************************************/
+/* GET byte                                                             */
+/************************************************************************/
+uint8_t flashGetByteDecode2(uint16_t pAddr) {
+	//read FLASH
+	DATABUS_READ_MODE;
+
+	//Doc: READ = /CE(l) & /WE(h) & /OE(l) & /RST(h)
+	PORT_CONTROL = PIN_CONTROLS_MASK & (PIN_RST | PIN_CE | PIN_OE);
+	_delay_us(1);
+
+	//WE goes high
+	PORT_CONTROL = PORT_CONTROL | PIN_WE;
+	_delay_us(1);
+
+	//put the address
+	PORT_ADDR_L = (uint8_t)pAddr;
+	PORT_ADDR_M = (uint8_t)(pAddr >> 8);
+	PORT_ADDR_H = PORT_ADDR_H & 0x3f;
+	_delay_us(1);
+
+	//CE goes low
+	PORT_CONTROL = PORT_CONTROL & !PIN_CE;
+	_delay_us(1);
+
+	//OE goes low
+	PORT_CONTROL = PORT_CONTROL & !PIN_OE;
+	//doc says tOE = 0ns
+	_delay_us(1);
+	
+	//read !!	
+	uint8_t vData = PINA;
+	
+	//OE & CE goes high
+	PORT_CONTROL = PORT_CONTROL | (PIN_OE | PIN_CE);
+	_delay_us(1);
+
+	//WE goes low
+	PORT_CONTROL = PORT_CONTROL & !PIN_WE;
+	_delay_us(1);
+	
+	
+#ifdef TALKATIVE
+
+	serialWriteString("GET addr: 0x");
+
+	char vBuffChar[6];
+	serialWriteString(itoa(pAddr, vBuffChar, 16));
+
+	serialWriteString(" > Value: 0x");
+
+	serialWriteString(itoa(vData, vBuffChar, 16));
+
+	serialWriteString("\r\n");
+#endif //TALKATIVE
+	return vData;
+}
+uint8_t flashGetByteDecode() {
+	uint16_t vAddress = flashSetAddressFromInput();
+	
+	return flashGetByteDecode2(vAddress);
+}
+
+
+/************************************************************************/
+/* WRITE byte                                                           */
+/* Input: -                                                             */
+/* Output: CONTROL = OE high                                            */
+/************************************************************************/
+void flashWriteSeq1Byte (uint16_t pAddr, uint8_t pData, uint8_t pIsLast){
+	//Doc: READ = /CE(l) & /WE(l) & /OE(h) & /RST(h)
+	//but watch the sequence
+	
+	//go write mode
+	DATABUS_WRITE_MODE;
+	
+	//Start : CE and WE high
+	PORT_CONTROL = PORT_CONTROL | (PIN_RST | PIN_CE | PIN_WE);
+	_delay_us(1);
+
+	//put the address
+	PORT_ADDR_L = (uint8_t)pAddr;
+	PORT_ADDR_M = (uint8_t)(pAddr >> 8);
+	PORT_ADDR_H = PORT_ADDR_H & 0x3f;
+	_delay_us(1);
+
+	//CE goes low
+	PORT_CONTROL = PORT_CONTROL & !PIN_CE;
+	_delay_us(1);
+
+	//OE goes high
+	PORT_CONTROL = PORT_CONTROL | PIN_OE;
+	_delay_us(1);
+	
+	//WE goes low
+	PORT_CONTROL = PORT_CONTROL & !PIN_WE;
+	_delay_us(1);
+	
+	//put the data
+	PORT_DATA = pData;
+	_delay_us(1);
+		
+	//WE goes high
+	PORT_CONTROL = PORT_CONTROL | PIN_WE;
+	_delay_us(1);	
+
+	if (pIsLast != 0){
+		//CE goes high
+		PORT_CONTROL = PORT_CONTROL | PIN_CE;
+		_delay_us(1);
+
+		//CE goes low
+		PORT_CONTROL = PORT_CONTROL & !PIN_CE;
+		_delay_us(1);
+
+		//OE goes low
+		PORT_CONTROL = PORT_CONTROL & !PIN_OE;
+		_delay_us(1);
+		
+	}
+}
+
+
+void flashWriteByteDecode2(uint16_t pAddress, uint8_t pData) {
+
+	PORT_CONTROL = PORT_CONTROL | (PIN_RST);
+	
+/*
+	//Byte Program 4 555h AAh AAAh 55h 555h A0h PA PD
+	flashWriteSeq1Byte(0x0555, 0xaa,0);
+	flashWriteSeq1Byte(0x0aaa, 0x55,0);
+	flashWriteSeq1Byte(0x0555, 0xa0,0);
+	flashWriteSeq1Byte(pAddress, pData,1);
+*/
+	
+	//5555 / aa
+	//2aaa / 55
+	//5555 / a0
+	//addr / data
+	
+	flashWriteSeq1Byte(0x5555, 0xaa, 0);
+	flashWriteSeq1Byte(0x2aaa, 0x55, 0);
+	flashWriteSeq1Byte(0x5555, 0xa0, 0);
+	flashWriteSeq1Byte(pAddress, pData, 1);
+	
+
+#ifdef TALKATIVE
+	serialWriteString("WRITE addr: ");
+
+	char vBuffChar[6];
+	serialWriteString(itoa(pAddress, vBuffChar, 16));
+
+	serialWriteString(" < Value: 0x");
+
+	serialWriteString(itoa(pData, vBuffChar, 16));
+
+	serialWriteString("\r\n");
+#endif //TALKATIVE
+}
+
+
+void flashWriteByteDecode() {
+	//address
+	uint16_t vAddress = flashSetAddressFromInput();
+	uint8_t vData = flashGetDataFromInput();
+
+	flashWriteByteDecode2(vAddress, vData);
+}
+
+/************************************************************************/
+/* Shows content of dataport                                            */
+/************************************************************************/
+
+#ifdef TALKATIVE
+void showDataPort() {
+	serialWriteString("Data port content: PORTA=0x");
+
+	char vBuffChar[6];
+	serialWriteString(itoa(PORTA, vBuffChar, 16));
+
+	serialWriteString(" PINA=0x");
+	serialWriteString(itoa(PINA, vBuffChar, 16));
+
+	serialWriteString("\r\n");
+}
+#endif //TALKATIVE
+
+/************************************************************************/
+/* DECODE command                                                       */
+/************************************************************************/
 void decodeCommand (){	
 	uint8_t inByte = 0;         // incoming serial byte
 	
-	while(1) {
-		if (serialCheckRxComplete()) {
-			inByte = serialRead();
+	if (serialCheckRxComplete()) {
+		inByte = serialRead();
 			
-			switch (inByte)
-			{
-				case 'g':
-				case 'G':
-				//GET a byte
-				//Format "gAAAAA" with AAA is address MSB first
-				flashGetByteDecode();
-				break;
-				default:
-				//Unknown command
+		switch (inByte)
+		{
+			case 'g':
+			case 'G':
+			//GET a byte
+			//Format "gAAAAA" with AAA is address MSB first
+			flashGetByteDecode();
+			break;
+
+			case 'w':
+			case 'W':
+			//WRITE a byte
+			//Format "gAAAAADD" with AAA is address and DD is data (MSB first)
+			flashWriteByteDecode();
+			break;					
+
 #ifdef TALKATIVE
-				serialWriteString("ERROR: unknown command: ");serialWrite(inByte);serialWriteString("\r\n");
+			case 'n':
+			//GET a byte for the test
+			flashGetByteDecode2(0x1111);
+			break;
+			case 'm':
+			//WRITE a byte for the test
+			flashWriteByteDecode2(0x1111, 0x67);
+			break;
+
+			case 'd':
+			case 'D':
+			//show content of data port
+			showDataPort();
+			break;			
+#endif //TALKATIVE
+
+			default:
+			//Unknown command
+#ifdef TALKATIVE
+			serialWriteString("ERROR: unknown command: ");serialWrite(inByte);serialWriteString("\r\n");
 #endif //TALKATIVE				
-				break;
-			}
+			break;
 		}
-	}	//End main loop.
+	}
 }
 
 /************************************************************************/
@@ -208,10 +444,18 @@ int main (void)
 
 	establishContact();  // send a byte to establish contact until Processing responds
 	
+#ifdef TALKATIVE
+	serialWriteString("[Commands: gAAAAA wAAAAADD d]\r\n");
+#endif //TALKATIVE
 
 	while(1) {
 		if (serialCheckRxComplete()) {
 			decodeCommand();
+			
+#ifdef TALKATIVE
+			serialWriteString("[Commands: gAAAAA wAAAAADD d]\r\n>");
+#endif //TALKATIVE
+
 		}
 	}	//End main loop.
 	return 0;
