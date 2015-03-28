c2fb36318937343e925da5294844200b690469cc
alainvizzini@gmail.com <alainvizzini@gmail.com@a4e8eee6-8666-d384-cd2c-cc5a62ec2564>
2015-03-28 07:04:12 +0000

    Added image of binary

--- trunk/Wordclock/upload.avrstudio.328.bat
+++ trunk/Wordclock/upload.avrstudio.328.bat
@@ -1,3 +1,3 @@
 REM "D:\Program Files (x86)\Atmel\Atmel Studio 6.0\avrdbg\atprogram.exe" --tool avrispmk2 --interface ISP --device atmega328p program --flash --format hex --verify --chiperase --file "D:\SYSTEM~1\Alan\AppData\Local\Temp\build2881423641788428917.tmp\Wordclock.cpp.hex"
-"D:\Program Files (x86)\Atmel\Atmel Studio 6.0\avrdbg\atprogram.exe" --tool avrispmk2 --interface ISP --device atmega328p program --flash --format hex --verify --chiperase --file "D:\SYSTEM~1\Alan\AppData\Local\Temp\build2147040866717190233.tmp\Wordclock.cpp.hex"
+"D:\Program Files (x86)\Atmel\Atmel Studio 6.0\avrdbg\atprogram.exe" --tool avrispmk2 --interface ISP --device atmega328p program --flash --format hex --verify --chiperase --file "D:\Subversion\Wordclock\images\Wordclock.Numitron-328-DS3231.hex"
 pause
