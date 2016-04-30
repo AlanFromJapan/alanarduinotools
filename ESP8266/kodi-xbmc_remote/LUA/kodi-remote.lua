e955fae8489a1a184b51251c810982f5eff5500a
AlanFromJapan <alanfromjapan@gmail.com>
2016-04-30 18:28:57 +0900

    Cleaned up version, remains to plug on arduino or something alike now.

--- ESP8266/kodi-xbmc_remote/LUA/kodi-remote.lua
+++ ESP8266/kodi-xbmc_remote/LUA/kodi-remote.lua
@@ -9,37 +9,36 @@ conn = nil
 conn=net.createConnection(net.TCP, 0) 
 
 -- show the retrieved web page
-print("DEBUG command="..command.."\r\n")
-
-conn:on("receive", function(conn, payload) 
-                       success = true
-                       print(string.sub(payload, 1, 500)) 
-                       end) 
+--conn:on("receive", 
+--	function(conn, payload) 
+		-- print(string.sub(payload, 1, 500)) 
+--	end) 
 
 -- when connected, request page (send parameters to a script)
-conn:on("connection", function(conn, payload) 
-                       print('\nTCP Connected')
-						httpReq = "GET "..command.." HTTP/1.1\r\n" --Get /xxxx HTTP/1.1 
-						.."Host: "..ip.."\r\n"
-						.."Authorization: Basic "..token.."\r\n"
-               		    .."Connection: close\r\n"
---                        .."Accept: text/html\r\n" 
---                        .."User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n" 
---						.."Pragma: no-cache\r\n"
---						.."Cache-Control: no-cache\r\n"
-                        .."\r\n" --FINAL is needed!
-						
-						print("DEBUG HTTP Req:\r\n" .. httpReq)
-                       conn:send(httpReq)
-                       end) 
-					   
+conn:on("connection", 
+	function(conn, payload) 
+		print('\nTCP Connected')
+		httpReq = "GET "..command.." HTTP/1.1\r\n" --Get /xxxx HTTP/1.1 
+		.."Host: "..ip.."\r\n"
+		.."Authorization: Basic "..token.."\r\n"
+		.."Connection: close\r\n"
+		.."\r\n" --FINAL is needed!
+
+		conn:send(httpReq)
+	end) 
+
 -- when disconnected, let it be known
-conn:on("disconnection", function(conn, payload) print('\nTCP Disconnected') end)
-                                             
+conn:on("disconnection", 
+	function(conn, payload) 
+		print('\nTCP Disconnected') 
+	end)
+
 conn:connect(port,ip) 
 
+--do not close manually
 --conn:close()
 --conn = nil
+
 
 end --/callKodiRemoteURL
 
