d58dd1462a6b4002d532e6d5a4c96d82d2897d22
Alan (BigOne01) <alanfromjapan@gmail.com>
2019-02-09 23:28:39 +0900

    test folder and files for weatherbit.io service

---
+++ Python/EinkWeatherStation/weatherbit.io.test/weatherbit_test.py
@@ -0,0 +1,12 @@
+import io
+import datetime
+import time
+
+#Tokyo
+CITYCODE="1850147"
+
+
+#placeholders for city and key
+URL_CURRENTWEATHER="https://api.weatherbit.io/v2.0/current?city_id=%s&units=S&key=%s"
+
+
