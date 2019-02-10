133e39d0ad2e21a1448e230181da6c4b952e6607
Alan (BigOne01) <alanfromjapan@gmail.com>
2019-02-11 00:27:40 +0900

    librairy dev continued, can do daily data and 2 days every 3h. will need code cleanup next before usage.

--- Python/EinkWeatherStation/weatherbit.io.test/weatherbit_test.py
+++ Python/EinkWeatherStation/weatherbit.io.test/weatherbit_test.py
@@ -1,12 +1,71 @@
 import io
 import datetime
 import time
+import json
+import requests
+import sys
+
+import weatherbitio as wbit
 
 #Tokyo
 CITYCODE="1850147"
 
 
 #placeholders for city and key
-URL_CURRENTWEATHER="https://api.weatherbit.io/v2.0/current?city_id=%s&units=S&key=%s"
+URL_CURRENTWEATHER="https://api.weatherbit.io/v2.0/current?city_id=%s&key=%s"
+URL_2DAYS3H="https://api.weatherbit.io/v2.0/forecast/3hourly?city_id=%s&days=1&key=%s"
 
 
+def showOne(weather):
+	print(weather)
+	print("City = " + weather["city_name"] if "city_name" in weather else "?")
+	print("Current temp  = " + str(weather["temp"]))
+	print("Current weather  = " + weather["weather"])
+	print("Current status = %s (%d)" % (weather["status"], weather["code"]))
+	print ("Time = %s" % (weather["time"]))
+
+
+def testToday(pKey):
+	print("**TODAY**")
+	result = wbit.getDaily(pKey, CITYCODE)
+	showOne(result)
+
+
+def loadJson(filename):
+    t = ""
+    with open(filename, 'r') as f:
+    	t = f.read()
+    return json.loads(t)
+
+def testNext (pKey):
+	print ("** TEST next **")
+
+	"""
+	j = loadJson("sample_next.json")
+	for d in j["data"]:
+		print ("%s temp %s : %s" % (d["timestamp_local"], d["temp"], d["weather"]["description"]))
+	"""
+
+	result = wbit.getNext(pKey, CITYCODE)
+	for n in result["data"]:
+		showOne(result["data"][n])
+		print ("-------------------------------------------------------------")
+
+
+def main(pKey, pData=""):
+	#testToday(pKey)
+	testNext(pKey)
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print("No key, will use sample data")
+        sample = ""
+        with open('sample_daily.json', 'r') as f:
+        	sample = f.read()
+
+        main(pKey=None, pData=sample)
+    else:
+    	vKey = str(sys.argv[1])
+    	main(pKey=vKey)
---
+++ Python/EinkWeatherStation/weatherbit.io.test/weatherbitio.py
@@ -0,0 +1,65 @@
+import json
+import requests
+
+#placeholders for city and key
+URL_CURRENTWEATHER="https://api.weatherbit.io/v2.0/current?city_id=%s&key=%s"
+URL_2DAYS3H="https://api.weatherbit.io/v2.0/forecast/3hourly?city_id=%s&days=1&key=%s"
+
+#Maps weather code to a easy to one word code
+code2weather = {
+	200: "Thunder",
+	300: "Drizzle",
+	500: "Rain",
+	600: "snow",
+	700: "Mist",
+	800: "Sunny",
+	900: "Unknown"
+
+}
+
+
+#pass the initialized dict in resp
+def unwrapOneData(resp, k):	
+	resp["temp"] = k["temp"]
+	resp["weather"] = k["weather"]["description"]
+
+	code = int(k["weather"]["code"])
+	code = (code / 100)*100
+
+	resp["code"] = code
+	resp["status"] = "Unknown" if not code in code2weather else code2weather[code]
+
+
+	resp["time"] = "now" if not "timestamp_local" in k else k["timestamp_local"]
+
+	return resp
+
+
+def getDaily(pKey, pCity):
+	response = requests.get(URL_CURRENTWEATHER % (pCity, pKey))
+	j  = json.loads(response.text)
+
+	resp = dict()
+	resp["city_name"] = j["data"][0]["city_name"]
+
+	unwrapOneData(resp, j["data"][0])
+
+	return resp
+
+
+def getNext(pKey, pCity):
+	response = requests.get(URL_2DAYS3H % (pCity, pKey))
+	j  = json.loads(response.text)
+    
+	resp = dict()
+	
+	resp["city_name"] = j["city_name"]
+	resp["data"] = dict()
+
+	for d in j["data"]:
+		n = unwrapOneData(dict(), d)
+
+		resp["data"][n["time"]] = n
+
+	
+	return resp
