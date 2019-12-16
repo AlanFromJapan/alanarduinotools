6c68355ea94809aa80a966e74bf5fa9503f07fe9
AlanFromJapan <Alan@BigOne01>
2019-12-16 23:52:51 +0900

    Changed the USB ID to hopefully avoid collision and set a nice USB name.

--- Attiny2313/UsbMiniMediaKeyboard/usbconfig.h
+++ Attiny2313/UsbMiniMediaKeyboard/usbconfig.h
@@ -239,7 +239,7 @@ PID dec (hex)  | VID dec (hex) | Description of use
  * own Vendor ID, define it here. Otherwise you use obdev's free shared
  * VID/PID pair. Be sure to read USBID-License.txt for rules!
  */
-#define  USB_CFG_DEVICE_ID       0xdf, 0x06
+#define  USB_CFG_DEVICE_ID       0xe0, 0x06
 /* This is the ID of the product, low byte first. It is interpreted in the
  * scope of the vendor ID. If you have registered your own VID with usb.org
  * or if you have licensed a PID from somebody else, define it here. Otherwise
@@ -263,8 +263,8 @@ PID dec (hex)  | VID dec (hex) | Description of use
  * obdev's free shared VID/PID pair. See the file USB-IDs-for-free.txt for
  * details.
  */
-#define USB_CFG_DEVICE_NAME     'A','l','a','n',' ','U','S','B',' ','v','o','l','u','m','e',' ','c','o','n','t','r','o','l','l','e','r'
-#define USB_CFG_DEVICE_NAME_LEN		26
+#define USB_CFG_DEVICE_NAME  'M','i','n','i',' ','m','e','d','i','a',' ','k','e','y','b','o','a','r','d'
+#define USB_CFG_DEVICE_NAME_LEN  19
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USB-IDs-for-free.txt before you assign a name if
  * you use a shared VID/PID.
