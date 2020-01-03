6b51945918ad599614cbf9af5cf6111181bf0e54
AlanFromJapan <Alan@BigOne01>
2020-01-03 23:11:20 +0900

    most of rewriting is style only, and a tempo to avoid initial false status read (risk of starting with relay closed)

--- Attiny13/SmartFootSwitch13/main.c
+++ Attiny13/SmartFootSwitch13/main.c
@@ -23,6 +23,9 @@
 #define PORT_RELAY 		PORTB2
 #define PORT_SWITCH		PORTB0
 
+//0 off, else on
+uint8_t mRelayStatus = 0;
+
 //Just for the test of leds ... will be removed by optimizer
 void test_Blink()
 {
@@ -42,22 +45,36 @@ void test_Blink()
 //Blink all leds once
 void blink(void) {
 	PORTB |= 1 << PORT_LED_CLAP;
-	PORTB &= ~(1 << PORT_LED_STATUS);
 
 	_delay_ms(50);
 
-	PORTB |= 1 << PORT_LED_STATUS;
 	PORTB &= ~(1 << PORT_LED_CLAP);
 
 	_delay_ms(50);
 
 	PORTB &= ~(1 << PORT_LED_CLAP);
-	PORTB &= ~(1 << PORT_LED_STATUS);
 }
 
 //Toggles the relay status
 void toggleRelay (){
-	PORTB ^= (1 << PORT_RELAY);
+	if (mRelayStatus == 0)
+		relayOn();
+	else
+		relayOff();
+}
+
+//Turns the relay OFF
+void relayOff() {
+	PORTB &= ~(1 << PORT_RELAY);
+	PORTB &= ~(1 << PORT_LED_STATUS);
+	mRelayStatus = 0;
+}
+
+//Turns the relay ON
+void relayOn (){
+	PORTB |= (1 << PORT_RELAY);
+	PORTB |= (1 << PORT_LED_STATUS);
+	mRelayStatus = 1;
 }
 
 int main(void)
@@ -68,21 +85,29 @@ int main(void)
 	// no clock pre-scaling
 	CLKPR = 0x00;
 
-	//Switch on PB0 only is input, the rest it output
-	DDRB = 0xFE;
 	//pullup on PB0
 	PORTB = 0x01;
+	//Switch on PB0 only is input, the rest it output
+	DDRB = 0xFE;
 
-	//just make sure pullups are NOT disabled
+
+	//make sure pullups are NOT disabled
 	MCUCR |= (0 << PUD);
+
+	//make sure we start with a relay OFF
+	relayOff();
+
+	//temporize or the relay often starts closed (ON) which NOT what I want
+	_delay_ms(500);
+	blink();
+	blink();
 
 	//main body loop
 	while(1) {
 		//if foot button pressed
 		if ((PINB & 0x01) != 0x01){
-			blink();
-
 			toggleRelay();
+			blink();
 
 			//debounce
 			_delay_ms(100);
