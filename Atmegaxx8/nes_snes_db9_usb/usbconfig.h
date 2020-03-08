845670cb97c0004e532de8b4a9687b4a98ffbcb5
AlanFromJapan <Alan@BigOne01>
2020-03-08 18:36:05 +0900

    USB D- is D4, like default of the PCB (solder bridge)

--- Atmegaxx8/nes_snes_db9_usb/usbconfig.h
+++ Atmegaxx8/nes_snes_db9_usb/usbconfig.h
@@ -28,7 +28,7 @@ rename it to "usbconfig.h". Then edit it accordingly.
 /* This is the port where the USB bus is connected. When you configure it to
  * "B", the registers PORTB, PINB and DDRB will be used.
  */
-#define USB_CFG_DMINUS_BIT      0
+#define USB_CFG_DMINUS_BIT      4
 /* This is the bit number in USB_CFG_IOPORT where the USB D- line is connected.
  * This may be any bit in the port.
  */
