15e1cf190437fda360300512c6276b93fd8397b1
AlanFromJapan <Alan@BigOne01>
2020-04-05 23:26:41 +0900

    ROM Upload, then download works and gives identical files! Success!!

--- Python/GBCartridgeClient/gbcartclient.py
+++ Python/GBCartridgeClient/gbcartclient.py
@@ -5,8 +5,14 @@ import os
 import time
 
 #length of the ROM to read in Bytes
-#READROM_LEN = 32768
-READROM_LEN = 256
+READROM_LEN = 32768
+
+#used for debug, set it to a high value to not bug you
+WRITEROM_MAXLEN = 32768
+
+#should the write ROM to EEPROM reset it?
+WRITEROM_RESETROMBEFORE = True
+
 
 def readser():
     reps = ser.readlines()
@@ -18,8 +24,9 @@ def prompt():
     print("Type a raw command, '>filename' to READ, '<filename' to WRITE, Q to quit:")
 
 
+#READS a ROM from the EEPROM and saves it to disk
 def readRom(p):
-    print ("Read %dB ROM to '%s'" % (READROM_LEN, p) )
+    print ("Read {0:,}B ROM to '{1}'".format(READROM_LEN, p) )
     cmd = 'l' + hex(READROM_LEN)[2:].zfill(4)  #skip the '0x', pas left with zeroes to be 4 char long always
     #print ("DBG: cmd = " + cmd)
     ser.write(bytearray(cmd, 'utf8')) #read 32k
@@ -31,34 +38,67 @@ def readRom(p):
             rep = r
         else:
             rep = rep + r
-        time.sleep(1)
+        time.sleep(0.1)
         if ser.in_waiting == 0:
             break
 
     print("... received data, starting to write to file...")
     i = 0
-    with open(p, "wb") as t:
+    with open(p, "wb") as f:
         while i < READROM_LEN:
             v = rep[i*3+1:i*3+3] #v has the hex value 
             b = int(v, 16)
-            print("DBG %d : %s -> %d" % (i, v, b))
-            #print (bytearray([b]))            
-            t.write(bytearray([b]))
+            #print("DBG %d : %s -> %d" % (i, v, b))
+            f.write(bytearray([b])) #must put the value in an array
             i = i + 1
 
     print("Read rom finished.")
 
 
 
+#Writes a rom from disk to EEPROM (slowly)
 def writeRom(p):
-    print ("TODO")
+    print ("Starting to write ROM '%s'" % (p))
+
+    #FIRST: you should have done an EEPROM reset    
+    if WRITEROM_RESETROMBEFORE:
+        print ("Reset ROM before writing.")
+        ser.write(b'Z')
+        #need to pause a bit, the erase takes 1-2 sec so be large
+        time.sleep(5)
+        print(ser.read(30).decode(), end="")
+        
+
+    #SECOND: place yourself at the start
+    print ("Set cursor to EEPROM position 0")
+    ser.write(b's0000')
+    time.sleep(0.5)
+    print(ser.read(30).decode(), end="")
+    
+    #LASTLY: start writing
+    print("Starting writing!")
+    with open(p, "rb") as f:
+        i = 0
+        while i < WRITEROM_MAXLEN: 
+            buf = f.read(1)
+            if buf == None:
+                break
+
+            cmd = "S" + hex(int(buf[0]))[2:].zfill(2).upper()
+            #print("DBG: [%s] %s -> %s " % (str(i).zfill(4), cmd, buf))
+            
+            ser.write(bytearray(cmd, 'utf8'))
+            time.sleep(0.05)
+            print(ser.read(30).decode(), end="")
+            time.sleep(0.01)
+
+            i = i+1
 
 
 
-#default 9600,8,N,1 and 3sec timeout
-ser = serial.Serial('/dev/ttyUSB0', timeout=3)  # open serial port
-try:
-    print(ser.name)         # check which port was really used
+
+# Does the command line prompt
+def commandLineUI():
     ser.write(b'?')     # write a string
     readser()
 
@@ -75,12 +115,37 @@ try:
         elif i[0] == "!":
             print("DBG runs '>toto.gb'")
             readRom("toto.gb")
+        elif i[0] == "#":
+            print("DBG runs '<MysticSnake-n.gb'")
+            writeRom("MysticSnake-n.gb")            
         else:
             ser.write(str.encode(i))
             
         readser()
         print()
 
+
+
+#default 9600,8,N,1 and 2sec timeout
+ser = serial.Serial('/dev/ttyUSB0', timeout=2)  # open serial port
+try:
+    print("Connected to " + ser.name)         # check which port was really used
+
+    #was there a command line parameter for testing?
+    if len(sys.argv) == 2:
+        param = str(sys.argv[1])
+        print ("Received parameter '%s'" % (param))
+        if param == "testread":
+            print("DBG runs '>toto.gb'")
+            readRom("toto.gb")
+        elif param == "testwrite":
+            print("DBG runs '<MysticSnake-n.gb'")
+            writeRom("MysticSnake-n.gb")    
+        else:
+            print("Dunno what to do, showing prompt")
+        
+    #normal mode
+    commandLineUI()
 
 finally :
     ser.close()             # close port
