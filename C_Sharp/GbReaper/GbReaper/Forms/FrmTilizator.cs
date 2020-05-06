c60793388650e17b627fb9064ba6647d131af0fb
Alan@Vm <alanfromjapan@gmail.com>
2020-05-07 00:06:28 +0900

    Added Floyd Steinberg dither for tilizator

--- C_Sharp/GbReaper/GbReaper/Forms/FrmTilizator.cs
+++ C_Sharp/GbReaper/GbReaper/Forms/FrmTilizator.cs
@@ -6,15 +6,22 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using FloydSteinberg;
 using GbReaper.Classes;
 
 namespace GbReaper.Forms {
     public partial class FrmTilizator : Form {
+
+        public const string METHOD_NEAREST = "Nearest color";
+        public const string METHOD_FLOYDSTEINBERG = "Floyd-Steinberg dither";
+
         public Bitmap CreateBmp { get { return (Bitmap)picPreviewA.Image; } }
         public int CreateWidth { get { return (int)undWidth.Value; } }
         public int CreateHeight { get { return (int)undHeight.Value; } }
         public int CreateTop { get { return (int)undTop.Value; } }
         public int CreateLeft { get { return (int)undLeft.Value; } }
+        public string ColorMethod { get { return (string)cbxMethod.SelectedItem; } }
+
 
         public FrmTilizator() {
             InitializeComponent();
@@ -37,6 +44,8 @@ namespace GbReaper.Forms {
         }
 
         private void GeneratePreview() {
+            if (string.IsNullOrEmpty(txbImage.Text))
+                return;
 
             Bitmap vBmp = new Bitmap(txbImage.Text);
             picPreviewA.Image = vBmp;
@@ -44,20 +53,43 @@ namespace GbReaper.Forms {
             Bitmap vBmpResized = DrawingLogic.CopyAndResize(vBmp, Tile.WIDTH_PX * this.CreateWidth, Tile.HEIGHT_PX * this.CreateHeight);
             picPreviewB.Image = vBmpResized;
 
-            Bitmap vBmpColorized = DrawingLogic.MapBitmapColorsToPalette((Bitmap)vBmpResized.Clone(), Palette.DEFAULT_PALETTE);
+            Bitmap vBmpColorized = null;
+            switch (cbxMethod.SelectedItem) {
+                case METHOD_NEAREST:
+                    vBmpColorized = DrawingLogic.MapBitmapColorsToPalette((Bitmap)vBmpResized.Clone(), Palette.DEFAULT_PALETTE);
+                    break;
+                case METHOD_FLOYDSTEINBERG:
+                    vBmpColorized = FloydSteinbergDither.Process((Bitmap)vBmpResized.Clone(), Palette.DEFAULT_PALETTE.mColors);
+                    break;
+
+            }
+            
             picPreviewC.Image = vBmpColorized;
         }
 
         private void FrmTilizator_Load(object sender, EventArgs e) {
             ucPaletteEditor1.CurrentPalette = Palette.DEFAULT_PALETTE;
+            cbxMethod.Items.Add(METHOD_NEAREST);
+            cbxMethod.Items.Add(METHOD_FLOYDSTEINBERG);
+            cbxMethod.SelectedIndex = 0;
         }
 
         private void undWidth_ValueChanged(object sender, EventArgs e) {
+            updateUsedTiles();
             GeneratePreview();
         }
 
         private void undHeight_ValueChanged(object sender, EventArgs e) {
+            updateUsedTiles();
             GeneratePreview();
+        }
+
+        private void cbxMethod_SelectedIndexChanged(object sender, EventArgs e) {
+            GeneratePreview();
+        }
+
+        private void updateUsedTiles() {
+            lblTileCount.Text = (this.CreateWidth * this.CreateHeight) + " tiles used";
         }
     }
 }
