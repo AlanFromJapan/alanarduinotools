cb8741978402067ae4665f134473d1d2be9a22c7
alanfromjapan <alanfromjapan@gmail.com>
2022-10-19 22:43:54 +0900

    Now accepts command line param (start number or char). as good as this needs to gets, it's finished.

--- Python/NumberCardsGenerator/numbercardgen.py
+++ Python/NumberCardsGenerator/numbercardgen.py
@@ -1,10 +1,14 @@
+from curses.ascii import isdigit
 from lib2to3.pgen2.token import STAR
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
-import os
+import sys
 
-FONT = "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf"
+#FONT = "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
+#FONT = "/usr/share/fonts/truetype/malayalam/Keraleeyam-Regular.ttf"
+#FONT = "/usr/share/fonts/truetype/ubuntu/Ubuntu-C.ttf"
+FONT = "/usr/share/fonts/opentype/urw-base35/NimbusSansNarrow-Bold.otf"
 
 #Params
 BLOCK_PER_LINE=5
@@ -14,12 +18,18 @@ SPACER_PX=20
 PAGE_WIDTH_PX=2100
 PAGE_HEIGHT_PX=2970
 
-START=1
+START=32
+if len(sys.argv) > 1:
+    param = sys.argv[1]
+    if param.isdigit():
+        START = int(param)
+    else:
+        START = str(param)
 
 ######################################################################
 #Globals
 #Fonts
-font_xbig = ImageFont.truetype(FONT, 256)
+font_xbig = ImageFont.truetype(FONT, 356)
 
 img = Image.new("RGB", (PAGE_WIDTH_PX, PAGE_HEIGHT_PX), (255,255,255))
 draw = ImageDraw.Draw(img)
