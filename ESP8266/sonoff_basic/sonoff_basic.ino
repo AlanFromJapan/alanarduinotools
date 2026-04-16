240a126cdcfd3b782fa101de99e0f74c80b7cac0
AlanFromJapan <alanfromjapan@gmail.com>
2026-04-16 23:26:08 +0900

    Rebuilt the Sonoff code with recent code, and added basic blink

--- ESP8266/sonoff_basic/sonoff_basic.ino
+++ ESP8266/sonoff_basic/sonoff_basic.ino
@@ -26,9 +26,9 @@ int gpio12Relay = 12;
 volatile byte mStatus = 0;
 
 void generatePage(){
-  webPage = "<h1>SONOFF Web Server 2</h1><div style=\"height:80px;width:80px;background-color:";
+  webPage = "<h1>SONOFF Web Server 2</h1><div style=\"height:300px;width:300px;background-color:";
   webPage += (mStatus == 0? "red": "green") ;
-  webPage += ";\"/><p><a href=\"on\"><button>ON</button></a>&nbsp;<a href=\"off\"><button>OFF</button></a></p>"; 
+  webPage += ";\"/><p><a href=\"on\"><button style=\"width:100px;\">ON</button></a>&nbsp;<a href=\"off\"><button style=\"width:100px;\">OFF</button></a></p>"; 
 }
 
 void setup(void){
@@ -42,32 +42,17 @@ void setup(void){
   digitalWrite(gpio12Relay, LOW); //relay is OPEN by default!
  
   Serial.begin(115200); 
-  delay(5000);
+  delay(1000);
   WiFi.begin(ssid, password);
   Serial.println("");
 
-uint8_t dummy = 0;
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED && dummy < 10) {
-    
-  digitalWrite(gpio13Led, LOW);
-  
-    //https://github.com/esp8266/Arduino/issues/2186
-    WiFi.persistent(false);
-    WiFi.mode(WIFI_OFF);
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, password);
-
+  // Wait for connection (blinking)
+  while (WiFi.status() != WL_CONNECTED) {    
+    digitalWrite(gpio13Led, LOW);
     delay(500);
-  digitalWrite(gpio13Led, HIGH);
+    digitalWrite(gpio13Led, HIGH);
     delay(500);
     Serial.print(".");
-    dummy++;
-  }
-
-  if (dummy == 10){
-    Serial.println("");
-    Serial.println("[Ok, assuming that we are connected...]");
   }
   
   Serial.println("");
@@ -76,7 +61,7 @@ uint8_t dummy = 0;
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
   
-  if (mdns.begin("esp8266", WiFi.localIP())) {
+  if (mdns.begin("sonoff2", WiFi.localIP())) {
     Serial.println("MDNS responder started");
   }
   
@@ -91,7 +76,6 @@ uint8_t dummy = 0;
     generatePage();
 
     server.send(200, "text/html", webPage);
-    delay(1000);
   });
   server.on("/off", [](){
     digitalWrite(gpio13Led, HIGH);
@@ -100,13 +84,33 @@ uint8_t dummy = 0;
     generatePage();
 
     server.send(200, "text/html", webPage);
-    delay(1000); 
   });
+
+/*
+  server.on("/favicon.ico", []() {
+    static const uint8_t gif[] PROGMEM = {
+      0x47, 0x49, 0x46, 0x38, 0x37, 0x61, 0x10, 0x00, 0x10, 0x00, 0x80, 0x01,
+      0x00, 0x00, 0x00, 0x00, 0xff, 0xff, 0xff, 0x2c, 0x00, 0x00, 0x00, 0x00,
+      0x10, 0x00, 0x10, 0x00, 0x00, 0x02, 0x19, 0x8c, 0x8f, 0xa9, 0xcb, 0x9d,
+      0x00, 0x5f, 0x74, 0xb4, 0x56, 0xb0, 0xb0, 0xd2, 0xf2, 0x35, 0x1e, 0x4c,
+      0x0c, 0x24, 0x5a, 0xe6, 0x89, 0xa6, 0x4d, 0x01, 0x00, 0x3b
+    };
+    char gif_colored[sizeof(gif)];
+    memcpy_P(gif_colored, gif, sizeof(gif));
+    // Set the background to a random set of colors
+    gif_colored[16] = millis() % 256;
+    gif_colored[17] = millis() % 256;
+    gif_colored[18] = millis() % 256;
+    server.send(200, "image/gif", gif_colored, sizeof(gif_colored));
+  });
+*/
+
   server.begin();
   Serial.println("HTTP server started");
 }
  
 void loop(void){
   server.handleClient();
+  mdns.update();
 } 
 
